136048fa4657c882173580163d60307444062aff
Harry Pierson <devhawk@users.noreply.github.com>
2018-10-16 12:06:13 -0700

    Python Milestone: Event support (#38)
    * add struct enum support
    
    * add Windows.Graphics.DirectX to test_python included namespaces + sort winmd files + namespaces
    
    * fix test case naming
    
    * stringmap events working
    
    * simplified C++ code gen by elimintating property and event special casing
    
    * optimize python signature for special method (get/put/add/remove)
    
    * remove bind_method_overloads and bind_special_name_method
    
    * rework in progress
    
    * get_required_interfaces working
    
    * WIP
    
    * WIP
    
    * update write_class_method_overload to use method_info
    
    * get_methods WIP
    
    * removed all use of MethodList from code_writers in favor of helper methods
    
    * finish projection of all required interface methods
    
    * rework collection test
    
    * re-enable QI for concrete interfaces via Python construction
    
    * CR Feedback
    
    * More CR feedback

--- src/test/python/testjson.py
+++ src/test/python/testjson.py
@@ -3,20 +3,20 @@ import find_projection
 import _pyrt
 import unittest
 
-class TestXlangJson(unittest.TestCase):
+class TestJson(unittest.TestCase):
 
     def test_activate_JsonArray(self):
         a = _pyrt.JsonArray()
-        self.assertEqual(a.Size, 0)
-        self.assertEqual(a.ValueType, 4)
+        self.assertEqual(a.get_Size(), 0)
+        self.assertEqual(a.get_ValueType(), 4)
         self.assertEqual(a.ToString(), "[]")
         self.assertEqual(a.Stringify(), "[]")
 
 
     def test_activate_JsonObject(self):
         o = _pyrt.JsonObject()
-        self.assertEqual(o.Size, 0)
-        self.assertEqual(o.ValueType, 5)
+        self.assertEqual(o.get_Size(), 0)
+        self.assertEqual(o.get_ValueType(), 5)
         self.assertEqual(o.ToString(), "{}")
         self.assertEqual(o.Stringify(), "{}")
 
@@ -30,23 +30,23 @@ class TestXlangJson(unittest.TestCase):
 
     def test_JsonArray_parse(self):
         a = _pyrt.JsonArray.Parse('[true, false, 42, null, [], {}, "plugh"]')
-        self.assertEqual(a.ValueType, 4)
-        self.assertEqual(a.Size, 7)
+        self.assertEqual(a.get_ValueType(), 4)
+        self.assertEqual(a.get_Size(), 7)
         self.assertTrue(a.GetBooleanAt(0))
         self.assertFalse(a.GetBooleanAt(1))
         self.assertEqual(a.GetNumberAt(2), 42)
         self.assertEqual(a.GetStringAt(6), "plugh")
 
         a2 = a.GetArrayAt(4)
-        self.assertEqual(a2.Size, 0)
+        self.assertEqual(a2.get_Size(), 0)
         o = a.GetObjectAt(5)
-        self.assertEqual(o.Size, 0)
+        self.assertEqual(o.get_Size(), 0)
 
     def test_JsonArray_GetView(self):
         a = _pyrt.JsonArray.Parse('[true, false, 42, null, [], {}, "plugh"]')
         view = a.GetView()
 
-        self.assertEqual(view.Size, 7)
+        self.assertEqual(view.get_Size(), 7)
         v0 = view.GetAt(0)
         self.assertTrue(v0.GetBoolean())
         v1 = view.GetAt(1)
@@ -58,56 +58,56 @@ class TestXlangJson(unittest.TestCase):
 
         v4 = view.GetAt(4)
         a4 = v4.GetArray()
-        self.assertEqual(a4.Size, 0)
+        self.assertEqual(a4.get_Size(), 0)
 
         v5 = view.GetAt(5)
         o5 = v5.GetObject()
-        self.assertEqual(o5.Size, 0)
+        self.assertEqual(o5.get_Size(), 0)
 
 
     def test_JsonValue_boolean(self):
         t = _pyrt.JsonValue.CreateBooleanValue(True)
-        self.assertEqual(t.ValueType, 1)
+        self.assertEqual(t.get_ValueType(), 1)
         self.assertTrue(t.GetBoolean())
 
         f = _pyrt.JsonValue.CreateBooleanValue(False)
-        self.assertEqual(f.ValueType, 1)
+        self.assertEqual(f.get_ValueType(), 1)
         self.assertFalse(f.GetBoolean())
 
     def test_JsonValue_null(self):
         n = _pyrt.JsonValue.CreateNullValue()
-        self.assertEqual(n.ValueType, 0)
+        self.assertEqual(n.get_ValueType(), 0)
 
     def test_JsonValue_number(self):
         t = _pyrt.JsonValue.CreateNumberValue(42)
-        self.assertEqual(t.ValueType, 2)
+        self.assertEqual(t.get_ValueType(), 2)
         self.assertEqual(t.GetNumber(), 42)
 
     def test_JsonValue_string(self):
         t = _pyrt.JsonValue.CreateStringValue("Plugh")
-        self.assertEqual(t.ValueType, 3)
+        self.assertEqual(t.get_ValueType(), 3)
         self.assertEqual(t.GetString(), "Plugh")
 
     def test_JsonValue_parse(self):
         b = _pyrt.JsonValue.Parse("true")
-        self.assertEqual(b.ValueType, 1)
+        self.assertEqual(b.get_ValueType(), 1)
         self.assertTrue(b.GetBoolean())
 
         n = _pyrt.JsonValue.Parse("16")
-        self.assertEqual(n.ValueType, 2)
+        self.assertEqual(n.get_ValueType(), 2)
         self.assertEqual(n.GetNumber(), 16)
 
         s = _pyrt.JsonValue.Parse("\"plugh\"")
-        self.assertEqual(s.ValueType, 3)
+        self.assertEqual(s.get_ValueType(), 3)
         self.assertEqual(s.GetString(), "plugh")
 
     def test_invalid_param_count_instance(self):
         a = _pyrt.JsonArray()
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(TypeError):
             a.Append(10, 20)
 
     def test_invalid_param_count_static(self):
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(TypeError):
             _pyrt.JsonArray.Parse(10, 20)
 
 
