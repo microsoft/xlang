5cef93cdb0b1832ecb5d0415241ba7887849023d
Harry Pierson <devhawk@users.noreply.github.com>
2018-10-24 07:38:39 -0700

    Fix ubuntu build (#48)
    * initial round of fixes to re-enable compiling on Ubuntu 18.04 via WSL.
    
    Note, compiling platform + platform tests on non-windows currently disabled
    
    * fix all platform errors
    
    * enable test_platform on non-windows (failing to link on Ubuntu 18.04)
    
    * update wslConfigureBionic to have LF line endings + update .gitattributes to ensure all .sh files only use LF
    
    * Add stub implementations of convert_string for non-win platforms
    
    Note, platform and test_platform now compile on Windows and Ubuntu 18.04, but 4 string tests related to conversion fail on Ubuntu

--- src/platform/pal_internal.h
+++ src/platform/pal_internal.h
@@ -5,7 +5,12 @@
 
 #ifdef _DEBUG
 
+#if XLANG_COMPILER_MSVC
 #define XLANG_ASSERT _ASSERTE
+#else
+#include <cassert>
+#define XLANG_ASSERT assert
+#endif
 #define XLANG_VERIFY XLANG_ASSERT
 #define XLANG_VERIFY_(result, expression) XLANG_ASSERT(result == expression)
 
@@ -30,6 +35,12 @@ namespace xlang
         throw xlang_error{ result };
     }
 
+// TODO: rework to_result to avoid exceptions warning on clang
+#if XLANG_COMPILER_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wexceptions"
+#endif
+
     inline xlang_result to_result() noexcept
     {
         try
@@ -45,4 +56,8 @@ namespace xlang
             return xlang_error_out_of_memory;
         }
     }
+
+#if XLANG_COMPILER_CLANG
+#pragma clang diagnostic pop
+#endif
 }
