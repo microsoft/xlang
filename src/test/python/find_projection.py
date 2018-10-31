2869eab21286c80e430d6e39d2af26cc7af61be7
Harry Pierson <hpierson@microsoft.com>
2018-10-31 11:18:44 -0700

    update find_projection to use installed pyrt if available, generated if not

--- src/test/python/find_projection.py
+++ src/test/python/find_projection.py
@@ -1,9 +1,5 @@
-from inspect import currentframe, getframeinfo
-from pathlib import Path
-import sys
-
-script_filename = getframeinfo(currentframe()).filename
-generated_path = (Path(script_filename).resolve().parent) / "generated"
-
-sys.path.append(str(generated_path))
-sys.path.append(str(generated_path / "build"))
+try:
+    import pyrt
+except ImportError:
+    import sys
+    sys.path.append("./generated")
