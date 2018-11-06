b9ae4882c5ddee7656bdc13a29edea6ea6e19641
Harry Pierson <hpierson@microsoft.com>
2018-11-06 09:14:02 -0800

    ubuntu build agent dockerfile cleanup

--- src/scripts/ubuntu/docker-build.cmd
+++ src/scripts/ubuntu/docker-build.cmd
@@ -1,3 +1,4 @@
-REM note, only @devhawk can run this script currently
-docker build -t devhawk/xlang-build .
-docker push  devhawk/xlang-build 
+docker build -t devhawk/xlang-ubuntu-build .
+
+REM note, only @devhawk can push this image to hub.docker.com/r/devhawk
+docker push devhawk/xlang-ubuntu-build 
