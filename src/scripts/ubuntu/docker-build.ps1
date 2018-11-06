9e5b1bfd996b1d8c72c077362a87d312cee58056
Harry Pierson <hpierson@microsoft.com>
2018-11-06 12:52:17 -0800

    change acr endpoint and remove docker build script

--- src/scripts/ubuntu/docker-build.ps1
+++
@@ -1,8 +0,0 @@
-param (
-    [string]$dockerImageRepoName = "xlang-ubuntu-build",
-    [string]$dockerContainerRegistry = "msftxlangtemp.azurecr.io"
-)
-
-docker build -t $dockerImageRepoName $PSScriptRoot
-docker tag $dockerImageRepoName "$dockerContainerRegistry/$dockerImageRepoName"
-docker push "$dockerContainerRegistry/$dockerImageRepoName"
