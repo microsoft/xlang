3e981b580d5429eb1b32236df1eede01e1b29a98
Harry Pierson <harrypierson@outlook.com>
2018-11-06 22:58:08 -0800

    rework python_test target cmake file + specify py limited api in generated setup.py

--- src/tool/python/strings/setup.py
+++ src/tool/python/strings/setup.py
@@ -6,12 +6,13 @@ import setuptools
 
 setuptools.setup(
     name = "%",
-    version = "1.0.alpha",
+    version = "1.0a0",
     description="Generated Python/WinRT package",
     license="MIT",
     url="http://github.com/Microsoft/xlang",
     ext_modules = [ setuptools.Extension('%', 
         sources = [%],
+        py_limited_api = True,
         extra_compile_args = ["/std:c++17", "/await", "/Zi", "/Od"],
         include_dirs = ['.'],
         extra_link_args=['/DEBUG'],
