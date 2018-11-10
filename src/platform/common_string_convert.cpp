ea981a72ed0e73acdb85c71e098e88659f46c16f
ryansh <ryansh@microsoft.com>
2018-11-09 19:47:50 -0800

    Fix some clang-tidy warnings

--- src/platform/common_string_convert.cpp
+++ src/platform/common_string_convert.cpp
@@ -14,24 +14,24 @@ namespace xlang::impl
     }
 
     uint32_t convert_string(
-        std::basic_string_view<char16_t> input_str,
-        xlang_char8* output_buffer,
-        uint32_t buffer_size)
+        std::basic_string_view<char16_t> /*input_str*/,
+        xlang_char8* /*output_buffer*/,
+        uint32_t /*buffer_size*/)
     {
         static_assert(sizeof(xlang_char8) == sizeof(char));
  
-        // TODO: implement convert_string for non-windows platforms (#47)
+        // TODO(#47): implement convert_string for non-windows platforms
         throw_result(xlang_error_sadness);
     }
     
     uint32_t convert_string(
-        std::basic_string_view<xlang_char8> input_str,
-        char16_t* output_buffer,
-        uint32_t buffer_size)
+        std::basic_string_view<xlang_char8> /*input_str*/,
+        char16_t* /*output_buffer*/,
+        uint32_t /*buffer_size*/)
     {
         static_assert(sizeof(xlang_char8) == sizeof(char));
 
-        // TODO: implement convert_string for non-windows platforms (#47)
+        // TODO(#47): implement convert_string for non-windows platforms
         throw_result(xlang_error_sadness);
     }
-}
+} // namespace xlang::impl
