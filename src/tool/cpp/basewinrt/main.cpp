c50f693616ece400ce5d000821b8802ec80951f5
Kenny Kerr <kekerr@microsoft.com>
2018-11-28 08:42:22 -0800

    clean

--- src/tool/cpp/basewinrt/main.cpp
+++ src/tool/cpp/basewinrt/main.cpp
@@ -2,9 +2,4 @@
 
 int main()
 {
-    using namespace winrt::Windows::Foundation;
-
-    EventHandler<int> a;
-    EventHandler<int> h = [](auto&&...){};
-    EventHandler<int> g = { nullptr, winrt::take_ownership_from_abi };
 }
--- src/tool/cpp/basex/main.cpp
+++ src/tool/cpp/basex/main.cpp
@@ -1,13 +1,5 @@
 #include "base.h"
 
-using namespace xlang;
-
-IAsyncOperation<hstring> Sample()
-{
-    co_return L"Hello xlang";
-}
-
 int main()
 {
-    printf("%ls\n", Sample().get().c_str());
 }
