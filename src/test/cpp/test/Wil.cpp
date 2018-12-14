a8d96bf5f084e33c54b0def3c6a497d58cb0708b
Kenny Kerr <kekerr@microsoft.com>
2018-12-14 10:10:22 -0800

    comments

--- src/test/cpp/test/Wil.cpp
+++ src/test/cpp/test/Wil.cpp
@@ -6,6 +6,7 @@ using namespace Windows::Foundation;
 
 namespace
 {
+    // Some custom exception type unknown to C++/WinRT
     struct WilException
     {
     };
@@ -14,10 +15,12 @@ namespace
     {
         hstring ToString()
         {
+            // Throw custom exception inside C++/WinRT projection
             throw WilException();
         }
     };
 
+    // Global handler to translate custom exception
     int32_t WINRT_CALL handler(void* address) noexcept
     {
         REQUIRE(address);
@@ -38,10 +41,13 @@ namespace
 
 TEST_CASE("Wil")
 {
+    // Set up global handler
     REQUIRE(!winrt_to_hresult_handler);
     winrt_to_hresult_handler = handler;
 
+    // Validate that handler translated exception
     REQUIRE_THROWS_AS(make<Sample>().ToString(), hresult_illegal_delegate_assignment);
 
+    // Remove global handler
     winrt_to_hresult_handler = nullptr;
 }
