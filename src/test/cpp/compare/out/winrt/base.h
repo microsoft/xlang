55410a6eea52c320e7bd17527be783c2f6e476bf
Kenny Kerr <kekerr@microsoft.com>
2019-01-02 12:09:38 -0800

    capture

--- src/test/cpp/compare/out/winrt/base.h
+++ src/test/cpp/compare/out/winrt/base.h
@@ -2487,16 +2487,15 @@ WINRT_EXPORT namespace winrt
     template <typename T, typename F, typename...Args>
     auto capture(F function, Args&&...args)
     {
-        com_ptr<T> result;
-        check_hresult(function(args..., guid_of<T>(), result.put_void()));
+        impl::com_ref<T> result{ nullptr };
+        check_hresult(function(args..., guid_of<T>(), reinterpret_cast<void**>(put_abi(result))));
         return result;
     }
-
     template <typename T, typename O, typename M, typename...Args>
     auto capture(com_ptr<O> const& object, M method, Args&&...args)
     {
-        com_ptr<T> result;
-        check_hresult((object.get()->*(method))(args..., guid_of<T>(), result.put_void()));
+        impl::com_ref<T> result{ nullptr };
+        check_hresult((object.get()->*(method))(args..., guid_of<T>(), reinterpret_cast<void**>(put_abi(result))));
         return result;
     }
 
