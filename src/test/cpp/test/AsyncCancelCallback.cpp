ad2f96d524a0c7209bce2ecde744bdf35c2f53db
Kenny Kerr <kekerr@microsoft.com>
2019-01-03 15:31:54 -0800

    More tests

---
+++ src/test/cpp/test/AsyncCancelCallback.cpp
@@ -0,0 +1,36 @@
+#include "catch.hpp"
+#include <windows.h>
+#include "winrt/Windows.Foundation.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+namespace
+{
+    IAsyncAction Action(HANDLE event)
+    {
+        co_await resume_on_signal(event);
+    }
+
+    IAsyncActionWithProgress<int> ActionWithProgress(HANDLE event)
+    {
+        co_await resume_on_signal(event);
+    }
+
+    IAsyncOperation<int> Operation(HANDLE event)
+    {
+        co_await resume_on_signal(event);
+        co_return 1;
+    }
+
+    IAsyncOperationWithProgress<int, int> OperationWithProgress(HANDLE event)
+    {
+        co_await resume_on_signal(event);
+        co_return 1;
+    }
+}
+
+TEST_CASE("AsyncCancelCallback")
+{
+
+}
