7c1c3eb20e4fccee27dc41294bf9f731907c354a
Kenny Kerr <kekerr@microsoft.com>
2019-01-03 17:09:32 -0800

    AsyncCancelCallback

--- src/test/cpp/test/AsyncCancelCallback.cpp
+++ src/test/cpp/test/AsyncCancelCallback.cpp
@@ -7,30 +7,84 @@ using namespace Windows::Foundation;
 
 namespace
 {
-    IAsyncAction Action(HANDLE event)
+    IAsyncAction Action(HANDLE event, bool& canceled)
     {
+        auto cancel = co_await get_cancellation_token();
+
+        cancel.callback([&]
+            {
+                REQUIRE(!canceled);
+                canceled = true;
+            });
+
         co_await resume_on_signal(event);
+        co_await std::experimental::suspend_never();
+        REQUIRE(false);
     }
 
-    IAsyncActionWithProgress<int> ActionWithProgress(HANDLE event)
+    IAsyncActionWithProgress<int> ActionWithProgress(HANDLE event, bool& canceled)
     {
+        auto cancel = co_await get_cancellation_token();
+
+        cancel.callback([&]
+            {
+                REQUIRE(!canceled);
+                canceled = true;
+            });
+
         co_await resume_on_signal(event);
+        co_await std::experimental::suspend_never();
+        REQUIRE(false);
     }
 
-    IAsyncOperation<int> Operation(HANDLE event)
+    IAsyncOperation<int> Operation(HANDLE event, bool& canceled)
     {
+        auto cancel = co_await get_cancellation_token();
+
+        cancel.callback([&]
+            {
+                REQUIRE(!canceled);
+                canceled = true;
+            });
+
         co_await resume_on_signal(event);
+        co_await std::experimental::suspend_never();
+        REQUIRE(false);
         co_return 1;
     }
 
-    IAsyncOperationWithProgress<int, int> OperationWithProgress(HANDLE event)
+    IAsyncOperationWithProgress<int, int> OperationWithProgress(HANDLE event, bool& canceled)
     {
+        auto cancel = co_await get_cancellation_token();
+
+        cancel.callback([&]
+            {
+                REQUIRE(!canceled);
+                canceled = true;
+            });
+
         co_await resume_on_signal(event);
+        co_await std::experimental::suspend_never();
+        REQUIRE(false);
         co_return 1;
+    }
+
+    template <typename F>
+    void Check(F make)
+    {
+        handle event{ CreateEvent(nullptr, true, false, nullptr) };
+        bool canceled = false;
+        auto async = make(event.get(), canceled);
+        async.Cancel();
+        REQUIRE(canceled);
+        SetEvent(event.get());
     }
 }
 
 TEST_CASE("AsyncCancelCallback")
 {
-
+    Check(Action);
+    Check(ActionWithProgress);
+    Check(Operation);
+    Check(OperationWithProgress);
 }
