34f04d96e6226504aaefd5104d4886bd7b90b019
Kenny Kerr <kekerr@microsoft.com>
2019-01-08 08:34:38 -0800

    AsyncThrow

--- src/test/cpp/test/AsyncThrow.cpp
+++ src/test/cpp/test/AsyncThrow.cpp
@@ -4,33 +4,55 @@
 
 using namespace winrt;
 using namespace Windows::Foundation;
+using namespace std::chrono_literals;
 
 namespace
 {
-    IAsyncAction Action(HANDLE event)
+    IAsyncAction Action()
     {
-        co_await resume_on_signal(event);
+        co_await 10ms;
+        throw hresult_invalid_argument(L"Async");
     }
 
-    IAsyncActionWithProgress<int> ActionWithProgress(HANDLE event)
+    IAsyncActionWithProgress<int> ActionWithProgress()
     {
-        co_await resume_on_signal(event);
+        co_await 10ms;
+        throw hresult_invalid_argument(L"Async");
     }
 
-    IAsyncOperation<int> Operation(HANDLE event)
+    IAsyncOperation<int> Operation()
     {
-        co_await resume_on_signal(event);
+        co_await 10ms;
+        throw hresult_invalid_argument(L"Async");
         co_return 1;
     }
 
-    IAsyncOperationWithProgress<int, int> OperationWithProgress(HANDLE event)
+    IAsyncOperationWithProgress<int, int> OperationWithProgress()
     {
-        co_await resume_on_signal(event);
+        co_await 10ms;
+        throw hresult_invalid_argument(L"Async");
         co_return 1;
+    }
+
+    template <typename F>
+    void Check(F make)
+    {
+        try
+        {
+            make().get();
+            REQUIRE(false);
+        }
+        catch (hresult_invalid_argument const& e)
+        {
+            REQUIRE(e.message() == L"Async");
+        }
     }
 }
 
 TEST_CASE("AsyncThrow")
 {
-
+    Check(Action);
+    Check(ActionWithProgress);
+    Check(Operation);
+    Check(OperationWithProgress);
 }
