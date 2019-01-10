c79e07a0e95d84244c3771fab42a64dc52480070
Kenny Kerr <kenny@kennykerr.ca>
2019-01-10 09:25:27 -0800

    C++/WinRT coroutine locals may return/complete before locals are destroyed (#116)

--- src/tool/cpp/cppwinrt/strings/base_std_async_operation.h
+++ src/tool/cpp/cppwinrt/strings/base_std_async_operation.h
@@ -4,52 +4,23 @@ WINRT_EXPORT namespace std::experimental
     template <typename TResult, typename... Args>
     struct coroutine_traits<winrt::Windows::Foundation::IAsyncOperation<TResult>, Args...>
     {
-        struct promise_type final : winrt::impl::promise_base<promise_type, winrt::Windows::Foundation::IAsyncOperation<TResult>,
+        struct promise_type final : winrt::impl::promise_base<promise_type,
+            winrt::Windows::Foundation::IAsyncOperation<TResult>,
             winrt::Windows::Foundation::AsyncOperationCompletedHandler<TResult>>
         {
-            using AsyncStatus = winrt::Windows::Foundation::AsyncStatus;
-
-            TResult GetResults()
+            TResult get_return_value() noexcept
             {
-                winrt::slim_lock_guard const guard(this->m_lock);
-
-                if (this->m_status == AsyncStatus::Completed)
-                {
-                    return m_result;
-                }
-
-                this->rethrow_if_failed();
-                WINRT_ASSERT(this->m_status == AsyncStatus::Started);
-                throw winrt::hresult_illegal_method_call();
+                return std::move(m_result);
             }
 
-            void return_value(TResult const& result)
+            void return_value(TResult&& value) noexcept
             {
-                winrt::Windows::Foundation::AsyncOperationCompletedHandler<TResult> handler;
-                AsyncStatus status;
+                m_result = std::move(value);
+            }
 
-                {
-                    winrt::slim_lock_guard const guard(this->m_lock);
-
-                    if (this->m_status == AsyncStatus::Started)
-                    {
-                        this->m_status = AsyncStatus::Completed;
-                        m_result = result;
-                    }
-                    else
-                    {
-                        WINRT_ASSERT(this->m_status == AsyncStatus::Canceled);
-                        this->m_exception = make_exception_ptr(winrt::hresult_canceled());
-                    }
-
-                    handler = std::move(this->m_completed);
-                    status = this->m_status;
-                }
-
-                if (handler)
-                {
-                    handler(*this, status);
-                }
+            void return_value(TResult const& value) noexcept
+            {
+                m_result = value;
             }
 
             TResult m_result{ winrt::impl::empty_value<TResult>() };
