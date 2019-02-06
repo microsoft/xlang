067009c5f2bec229bec1a23f88174ffc8a756096
Harry Pierson <hpierson@microsoft.com>
2019-02-05 19:07:48 -0800

    update factory adapter comment

--- src/test/component/xlang_factory_adapter.cpp
+++ src/test/component/xlang_factory_adapter.cpp
@@ -1,12 +1,9 @@
 #include "pal.h"
 #include "winrt/base.h"
 
-
-    // XLANG_PAL_EXPORT xlang_error_info* XLANG_CALL xlang_get_activation_factory(
-    //     xlang_string class_name,
-    //     xlang_guid const& iid,
-    //     void** factory
-    // ) XLANG_NOEXCEPT;
+// test_component_dll is a WinRT component. xlang's get activation factory has
+// a different signature, so we need to adapt xlang's function to winrt's until
+// we have a C++/xlang projection.
 
 extern int32_t WINRT_CALL WINRT_GetActivationFactory(void* classId, void** factory) noexcept;
 
