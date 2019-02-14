8eea2f09c2794fc04a4c34a48fdbcb56e2ef1e59
Ryan Shepherd <ryansh@microsoft.com>
2019-02-13 23:38:44 -0800

    Non-Windows Unicode string conversion

--- src/platform/string_traits.h
+++ src/platform/string_traits.h
@@ -2,24 +2,20 @@
 
 #include <stdint.h>
 
-template <typename char_type>
-struct alternate_type;
-
-template <>
-struct alternate_type<xlang_char8>
+namespace xlang::impl
 {
-    using result_type = char16_t;
-};
+    template <typename char_type>
+    struct alternate_type;
 
-template <>
-struct alternate_type<char16_t>
-{
-    using result_type = xlang_char8;
-};
+    template <>
+    struct alternate_type<xlang_char8>
+    {
+        using result_type = char16_t;
+    };
 
-template <typename char_type>
-struct buffer_info
-{
-    char_type const* buffer;
-    uint32_t length;
-};
+    template <>
+    struct alternate_type<char16_t>
+    {
+        using result_type = xlang_char8;
+    };
+}
