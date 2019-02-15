ba34a92d52784b8cacf1fbea6d68326dd01c2f7c
Harry Pierson <hpierson@microsoft.com>
2019-02-15 14:28:15 -0800

    modify xlang_factory_adapter to use PAL error helper

--- src/test/component/xlang_factory_adapter.cpp
+++ src/test/component/xlang_factory_adapter.cpp
@@ -1,4 +1,5 @@
 #include "pal.h"
+#include "pal_error.h"
 #include "winrt/base.h"
 
 // test_component_dll is a WinRT component. xlang's get activation factory has
@@ -10,10 +11,18 @@ extern int32_t WINRT_CALL WINRT_GetActivationFactory(void* classId, void** facto
 xlang_error_info* XLANG_CALL XLANG_get_activation_factory(xlang_string class_name, xlang_guid const& iid, void** factory)  XLANG_NOEXCEPT
 {
     winrt::Windows::Foundation::IUnknown temp_factory{};
-    winrt::check_hresult(WINRT_GetActivationFactory(class_name, winrt::put_abi(temp_factory)));
+    auto result = WINRT_GetActivationFactory(class_name, winrt::put_abi(temp_factory));
+    if (result < 0)
+    {
+        return xlang::originate_error(result);
+    }
 
     xlang_unknown* unk = (xlang_unknown*)winrt::get_abi(temp_factory);
-    winrt::check_hresult(unk->QueryInterface(iid, factory));
+    result = unk->QueryInterface(iid, factory);
+    if (result < 0)
+    {
+        return xlang::originate_error(result);
+    }
 
     return xlang_error_ok;
 }
