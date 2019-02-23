f66fc7d5854309ed96caae6055daa95b25f2ee6f
Scott Jones <scottj1s@gmail.com>
2019-02-22 17:00:53 -0800

    Final preparations for VS 16 update (#187)
    * Picking up a few remaining changes from internal repo
    
    * worker process remote support (x86 & x64 visualizers)
    
    * Support building for both VS15 and VS16
    
    * Update visualizer proejct to dev16 (NuGet VS SDK packages)
    
    * remove vs sdk condition, build natvis for both arches, for vsix
    
    * remove cruft
    
    * pull nuget before using
    
    * remove unnecessary ConcordPackageVersion tracking
    
    * moved natvis to package folder, migrated vsix.csproj to using
    Microsoft.VSSDK.BuildTools NuGet, instead of depending on VS SDK MSI
    
    * backed out xlang_build_arch
    
    * Moved to .NET 4.7.2 to match SDK 17663 in Windows build image
    
    * reverting back to 4.5.2, which windows image now has
    
    * Added missing microsoft.vssdk.targets import

--- src/package/natvis/pch.h
+++ src/package/natvis/pch.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX
 
 #include <windows.h>
 #include <vsdebugeng.h>
@@ -10,6 +11,7 @@
 #include <rometadataapi.h>
 #include <rometadata.h>
 #include <filesystem>
+#include <functional>
 #include <optional>
 #include <set>
 #include <memory>
