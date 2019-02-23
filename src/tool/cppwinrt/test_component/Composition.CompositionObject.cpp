6b195a9dcc9f0ab5aee83ff5a63e7f9fd14f3dc6
Kenny Kerr <kekerr@microsoft.com>
2019-02-22 20:16:26 -0800

    composition

---
+++ src/tool/cppwinrt/test_component/Composition.CompositionObject.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "Composition.CompositionObject.h"
+#include "Composition.CompositionObject.g.cpp"
+
+namespace winrt::test_component::Composition::implementation
+{
+    void CompositionObject::Close()
+    {
+    }
+
+    void CompositionObject::Compositor()
+    {
+    }
+
+    void CompositionObject::StartAnimationGroup()
+    {
+    }
+}
