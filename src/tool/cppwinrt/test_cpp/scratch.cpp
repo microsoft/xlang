6e79189e10b9f3a4588b680f99a47097199fb1b1
Kenny Kerr <kekerr@microsoft.com>
2019-02-27 12:46:31 -0800

    scratch

--- src/tool/cppwinrt/test_cpp/scratch.cpp
+++ src/tool/cppwinrt/test_cpp/scratch.cpp
@@ -1,26 +1,8 @@
 #include "pch.h"
 #include "meta_reader.h"
-#include <iostream>
 
 using namespace xlang::meta::reader;
 
 TEST_CASE("scratch")
 {
-    std::vector<std::string> files
-    {
-        R"(c:\windows\system32\WinMetadata\Windows.UI.winmd)",
-        R"(c:\windows\system32\WinMetadata\Windows.Storage.winmd)"
-    };
-
-    cache c(files);
-
-    for (auto&& [ns, members] : c.namespaces())
-    {
-        std::cout << ns << '\n';
-
-        for (auto&& type : members.classes)
-        {
-            std::cout << '\t' << type.TypeName() << '\n';
-        }
-    }
 }
