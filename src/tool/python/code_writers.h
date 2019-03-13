f79bda82d147e285c9a9b9b6a9b0c494eaa4a575
Harry Pierson <hpierson@microsoft.com>
2019-03-13 09:49:14 -0700

    temporarily append method index to disambiguate all methods

--- src/tool/python/code_writers.h
+++ src/tool/python/code_writers.h
@@ -288,6 +288,9 @@ static void _dealloc_@(%* self)
                 w.write(method.Name());
             }
         }
+
+        // temporarily append method index to disambiguate all methods
+        w.write(std::to_string(method.index()));
     }
 
     enum class arg_count
@@ -360,42 +363,42 @@ static void _dealloc_@(%* self)
 
     void write_method_table(writer& w, TypeDef const& type)
     {
-        for (auto&& attrib : type.CustomAttribute())
-        {
-            auto pair = attrib.TypeNamespaceAndName();
-            if (pair.second == "ActivatableAttribute" && pair.first == "Windows.Foundation.Metadata")
-            {
-                auto fixed_args = attrib.Value().FixedArgs();
-                auto elem0 = std::get<ElemSig>(fixed_args[0].value);
+        //for (auto&& attrib : type.CustomAttribute())
+        //{
+        //    auto pair = attrib.TypeNamespaceAndName();
+        //    if (pair.second == "ActivatableAttribute" && pair.first == "Windows.Foundation.Metadata")
+        //    {
+        //        auto fixed_args = attrib.Value().FixedArgs();
+        //        auto elem0 = std::get<ElemSig>(fixed_args[0].value);
 
-                // if the first param is SystemType, it holds the name of a factory interface
-                if (std::holds_alternative<ElemSig::SystemType>(elem0.value))
-                {
-                    auto factory_type = type.get_cache().find_required(
-                        std::get<ElemSig::SystemType>(elem0.value).name);
+        //        // if the first param is SystemType, it holds the name of a factory interface
+        //        if (std::holds_alternative<ElemSig::SystemType>(elem0.value))
+        //        {
+        //            auto factory_type = type.get_cache().find_required(
+        //                std::get<ElemSig::SystemType>(elem0.value).name);
 
-                    for (auto&& factory_method : factory_type.MethodList())
-                    {
-                        w.write("// Factory %.%.%\n", factory_type.TypeNamespace(), factory_type.TypeName(), factory_method.Name());
-                    }
-                }
-                else
-                {
-                    w.write("// %.%.default_ctor\n", type.TypeNamespace(), type.TypeName());
-                }
-            }
-            if (pair.second == "StaticAttribute" && pair.first == "Windows.Foundation.Metadata")
-            {
-                auto fixed_args = attrib.Value().FixedArgs();
-                auto elem0 = std::get<ElemSig>(fixed_args[0].value);
-                auto static_type = type.get_cache().find_required(std::get<ElemSig::SystemType>(elem0.value).name);
+        //            for (auto&& factory_method : factory_type.MethodList())
+        //            {
+        //                w.write("// Factory %.%.%\n", factory_type.TypeNamespace(), factory_type.TypeName(), factory_method.Name());
+        //            }
+        //        }
+        //        else
+        //        {
+        //            w.write("// %.%.default_ctor\n", type.TypeNamespace(), type.TypeName());
+        //        }
+        //    }
+        //    if (pair.second == "StaticAttribute" && pair.first == "Windows.Foundation.Metadata")
+        //    {
+        //        auto fixed_args = attrib.Value().FixedArgs();
+        //        auto elem0 = std::get<ElemSig>(fixed_args[0].value);
+        //        auto static_type = type.get_cache().find_required(std::get<ElemSig::SystemType>(elem0.value).name);
 
-                for (auto&& static_method : static_type.MethodList())
-                {
-                    w.write("// Static %.%.%\n", static_type.TypeNamespace(), static_type.TypeName(), static_method.Name());
-                }
-            }
-        }
+        //        for (auto&& static_method : static_type.MethodList())
+        //        {
+        //            w.write("// Static %.%.%\n", static_type.TypeNamespace(), static_type.TypeName(), static_method.Name());
+        //        }
+        //    }
+        //}
 
         for (auto&& ii : get_required_interfaces(type))
         {
