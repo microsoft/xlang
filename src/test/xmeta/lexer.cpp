bb996d9f57510074c7c168f82011b890f3797267
Will Thant <kythant@microsoft.com>
2019-03-15 14:59:59 -0700

    Chkpoint

---
+++ src/test/xmeta/lexer.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include <iostream>
+
+#include "antlr4-runtime.h"
+#include "XlangParser.h"
+#include "XlangLexer.h"
+
+#include <Windows.h>
+
+using namespace antlr4;
+
+#pragma execution_character_set("utf-8")
+
+TEST_CASE("Simple lexer")
+{
+    std::ifstream stream;
+    stream.open("../example/namespace_test.idl");
+
+    ANTLRInputStream input(stream);
+    XlangLexer lexer(&input);       
+    CommonTokenStream tokens(&lexer);
+    XlangParser parser(&tokens);
+
+    REQUIRE(parser.getNumberOfSyntaxErrors() == 0);
+}
+
--- src/tool/xmeta/main.cpp
+++ src/tool/xmeta/main.cpp
@@ -1,21 +1,8 @@
-/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
- * Use of this file is governed by the BSD 3-clause license that
- * can be found in the LICENSE.txt file in the project root.
- */
-
-
-//
-//  main.cpp
-//  antlr4-cpp-demo
-//
-//  Created by Mike Lischke on 13.03.16.
-//
-
 #include <iostream>
 
 #include "antlr4-runtime.h"
-#include "cpp/generated/XlangParser.h"
-#include "cpp/generated/XlangLexer.h"
+#include "XlangParser.h"
+#include "XlangLexer.h"
 
 #include <Windows.h>
 
