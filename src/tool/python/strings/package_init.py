8bde723da4000849aa152c4d1a81ffc1caa299fa
Harry Pierson <hpierson@microsoft.com>
2019-03-28 11:34:40 -0700

    dealloc + minor typo stuff

--- src/tool/python/strings/package_init.py
+++ src/tool/python/strings/package_init.py
@@ -1,13 +1,13 @@
 import _%
 _%.init_apartment()
 
-_missing = object()
-
 def _import_ns_module(ns):
     import importlib.machinery
     import importlib.util
+
     try:
         module_name = "_%_" + ns.replace('.', '_')
+
         loader = importlib.machinery.ExtensionFileLoader(module_name, _%.__file__)
         spec = importlib.util.spec_from_loader(module_name, loader)
         module = importlib.util.module_from_spec(spec)
