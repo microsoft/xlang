b05afbcc733de158e2b7f5bfe843dc7cfc4d793c
Will Thant <kythant@microsoft.com>
2019-04-10 12:07:52 -0700

    Improvement to midl3 test

--- src/test/xmeta/testmidlcompat.cpp
+++ src/test/xmeta/testmidlcompat.cpp
@@ -17,34 +17,38 @@ TEST_CASE("Midl3 compat test", "[!hide]") {
     std::string path = std::filesystem::current_path().string() + "\\midl3test";
     std::cout << "Starting test on Midl3 Idl Files" << std::endl;
     int num_errors = 0;
-    try {
-        for (const auto & entry : std::filesystem::directory_iterator(path))
-        {
-            std::cout << "Parsing File:" << std::endl;
-            std::cout << entry.path() << std::endl;
-            std::ifstream stream;
-            stream.open(entry.path());
-
-            ANTLRInputStream input(stream);
-            XlangLexer lexer(&input);
-            CommonTokenStream tokens(&lexer);
-            XlangParser parser(&tokens);
-
-            xlangtestlistener listener;
-            tree::ParseTree *tree = parser.xlang();
-            tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-            if (parser.getNumberOfSyntaxErrors() != 0)
-            {
-                num_errors++;
-                std::cout << "Syntax errors: " << parser.getNumberOfSyntaxErrors() << std::endl;
-            }
-        }
+    std::filesystem::directory_iterator iterator;
+    try 
+    {
+        iterator = std::filesystem::directory_iterator(path);
     }
     catch (...)
     {
         std::cout << "Cannot find Midl3 Idl Files. They might not be avaibale on your machine." << std::endl;
         std::cout << "They might not be avaibale on your machine because the files are internal only." << std::endl;
         std::cout << "A subset of these test files may become avaibale at a later time" << std::endl;
+    }
+
+    for (const auto & entry : iterator)
+    {
+        std::cout << "Parsing File:" << std::endl;
+        std::cout << entry.path() << std::endl;
+        std::ifstream stream;
+        stream.open(entry.path());
+
+        ANTLRInputStream input(stream);
+        XlangLexer lexer(&input);
+        CommonTokenStream tokens(&lexer);
+        XlangParser parser(&tokens);
+
+        xlangtestlistener listener;
+        tree::ParseTree *tree = parser.xlang();
+        tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+        if (parser.getNumberOfSyntaxErrors() != 0)
+        {
+            num_errors++;
+            std::cout << "Syntax errors: " << parser.getNumberOfSyntaxErrors() << std::endl;
+        }
     }
     REQUIRE(num_errors == 0);
 }
