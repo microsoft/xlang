fa327ea6f678fa06c42aae230f65496a80bb8a3c
kythant <48363984+kythant@users.noreply.github.com>
2019-04-12 11:40:31 -0700

    Parser test and changes for midl3 compatibility (#296)
    * initial commit
    
    * Class member name changed for consistency]\
    
    * tests for enum and improvements to the test
    
    * Changes to be compat with most of midl3
    
    * Grammar Revisions to support enum names that are property keywords
    
    * Added attribute declaration
    
    * Added test to parse all midl3 files
    
    * Hide the midl3 test
    
    * Putting the idl files on another commit
    
    * Error messages when the test can't find the midl3 files
    
    * Fix Enum illelgal assignments test
    
    * Improvement to midl3 test
    
    * return
    
    * Code review
    
    * one more thing
    
    * defferentiate interface member declaration with class member declaration since interface member does not have modifiers
    
    * reference

--- src/test/xmeta/testlexer.cpp
+++ src/test/xmeta/testlexer.cpp
@@ -4,29 +4,56 @@
 #include "antlr4-runtime.h"
 #include "XlangParser.h"
 #include "XlangLexer.h"
-#include "XlangBasicListener.h"
+#include "xlang_test_listener.h"
 #include <Windows.h>
 
 using namespace antlr4;
 
 #pragma execution_character_set("utf-8")
 
+/* 
+    Lexer test
+    Primarily test whether the antlr lexer is correctly tokenizing the string and the listener can 
+    store the correctly string. 
+*/
+
+
+/*
+    setup_and_run_parser
+
+    This helper method sets up the tokenizer and parser from the idl string and
+    walks through the AST with the xlang_test_listener. The test listener is used to checked
+    whether the idk string was lexed and parsed correctly by simply adding the string to a 
+    set which we check inside the check. This method also returns the number of syntax errors. 
+*/
+int setup_and_run_parser(std::string const& idl, xlang_test_listener &listener, bool disable_error_reporting = false)
+{
+    ANTLRInputStream input(idl);
+    XlangLexer lexer(&input);
+
+    CommonTokenStream tokens(&lexer);
+    XlangParser parser(&tokens);
+
+    if (disable_error_reporting) {
+        lexer.removeErrorListeners();
+        parser.removeErrorListeners();
+    }
+
+    tree::ParseTree *tree = parser.xlang();
+    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+    return parser.getNumberOfSyntaxErrors();
+}
+
 TEST_CASE("Namespace Identifier")
 {   
     std::string test_idl =
         "namespace test{}";
-    ANTLRInputStream input(test_idl);
-    XlangLexer lexer(&input);       
-    CommonTokenStream tokens(&lexer);
-    XlangParser parser(&tokens);
-    XlangBasicListener listener;
+        
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener) == 0);
 
-    tree::ParseTree *tree = parser.xlang();
-    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-    std::set<std::string> namespaces = listener.namespaces;
-
+    std::set<std::string> &namespaces = listener.namespaces;
     REQUIRE(namespaces.find("test") != namespaces.end());
-    REQUIRE(parser.getNumberOfSyntaxErrors() == 0);
 }
 
 TEST_CASE("Token identifier with unicode letter character")
@@ -39,15 +66,9 @@ TEST_CASE("Token identifier with unicode letter character")
         namespace test5ªကညￜ {} \
         namespace test6ᛮⅫⅯ {}";
 
-    ANTLRInputStream input(test_idl);
-    XlangLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    XlangParser parser(&tokens);
-    XlangBasicListener listener;
-
-    tree::ParseTree *tree = parser.xlang();
-    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-    std::set<std::string> namespaces = listener.namespaces;
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener) == 0);
+    std::set<std::string> &namespaces = listener.namespaces;
 
     REQUIRE(namespaces.find("test1AÆĦǆＺ") != namespaces.end()); // LU 
     REQUIRE(namespaces.find("test2aăɶｚ") != namespaces.end()); // LL
@@ -55,25 +76,14 @@ TEST_CASE("Token identifier with unicode letter character")
     REQUIRE(namespaces.find("test4ʰˀﾟ") != namespaces.end()); // LM
     REQUIRE(namespaces.find("test5ªကညￜ") != namespaces.end()); // LO
     REQUIRE(namespaces.find("test6ᛮⅫⅯ") != namespaces.end()); // NL
-    REQUIRE(parser.getNumberOfSyntaxErrors() == 0);
 }
 
 TEST_CASE("Identifer not starting with letter character")
 {
     std::string test_idl =
         "namespace 123abc {}";
-
-    ANTLRInputStream input(test_idl);
-    XlangLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    XlangParser parser(&tokens);
-    XlangBasicListener listener;
-
-    tree::ParseTree *tree = parser.xlang();
-    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-    std::set<std::string> namespaces = listener.namespaces;
-
-    REQUIRE(parser.getNumberOfSyntaxErrors() == 1);
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener, true) != 0);
 }
 
 TEST_CASE("Remove comments")
@@ -83,20 +93,13 @@ TEST_CASE("Remove comments")
         namespace test2 {} /* this is a \n multiline comment */ \n \
         namespace test3 {}";
 
-    ANTLRInputStream input(test_idl);
-    XlangLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    XlangParser parser(&tokens);
-    XlangBasicListener listener;
-                
-    tree::ParseTree *tree = parser.xlang();
-    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-    std::set<std::string> namespaces = listener.namespaces;
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener) == 0);
+    std::set<std::string> &namespaces = listener.namespaces;
 
     REQUIRE(namespaces.find("test") != namespaces.end());
     REQUIRE(namespaces.find("test2") != namespaces.end());
     REQUIRE(namespaces.find("test3") != namespaces.end());
-    REQUIRE(parser.getNumberOfSyntaxErrors() == 0);
 }
 
 TEST_CASE("Spacing")
@@ -106,20 +109,13 @@ TEST_CASE("Spacing")
         namespace   test2  \t {} \
         namespace    test3  \v {}";
 
-    ANTLRInputStream input(test_idl);
-    XlangLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    XlangParser parser(&tokens);
-    XlangBasicListener listener;
-
-    tree::ParseTree *tree = parser.xlang();
-    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-    std::set<std::string> namespaces = listener.namespaces;
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener) == 0);
+    std::set<std::string> &namespaces = listener.namespaces;
 
     REQUIRE(namespaces.find("test") != namespaces.end());
     REQUIRE(namespaces.find("test2") != namespaces.end());
     REQUIRE(namespaces.find("test3") != namespaces.end());
-    REQUIRE(parser.getNumberOfSyntaxErrors() == 0);
 }
 
 
@@ -133,16 +129,74 @@ TEST_CASE("Lexer uuid")
             delegate void WebAccountProviderCommandInvokedHandler(WebAccountProviderCommand command); \
         }";
 
-    ANTLRInputStream input(test_idl);
-    XlangLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-    XlangParser parser(&tokens);
-    XlangBasicListener listener;
-
-    tree::ParseTree *tree = parser.xlang();
-    tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
-    std::set<std::string> expressions = listener.expressions;
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener) == 0);
+    std::set<std::string> &expressions = listener.expressions;
 
     REQUIRE(expressions.find("b7de5527-4c8f-42dd-84da-5ec493abdb9a") != expressions.end());
-    REQUIRE(parser.getNumberOfSyntaxErrors() == 0);
+}
+
+TEST_CASE("Enum assignments")
+{
+    std::string test_idl =
+        "namespace Windows.Test \
+        { \
+            enum Color \
+            { \
+                Red, \
+                Green, \
+                Blue \
+            } \
+            enum Alignment \
+            { \
+                Center = 0, \
+                Right = 1 \
+            } \
+            enum Permissions \
+            { \
+                None = 0x0000, \
+                Camera = 0x0001, \
+                Microphone = 0x0002, \
+            } \
+        }";
+
+    xlang_test_listener listener;
+    REQUIRE(setup_and_run_parser(test_idl, listener) == 0);
+    std::set<std::string> &enums = listener.enums;
+
+    REQUIRE(enums.find("Red") != enums.end());
+    REQUIRE(enums.find("Green") != enums.end());
+    REQUIRE(enums.find("Blue") != enums.end());
+    REQUIRE(enums.find("Center") != enums.end());
+    REQUIRE(enums.find("Right") != enums.end());
+    REQUIRE(enums.find("None") != enums.end());
+    REQUIRE(enums.find("Camera") != enums.end());
+    REQUIRE(enums.find("Blue") != enums.end());
+    REQUIRE(enums.find("0") != enums.end());
+    REQUIRE(enums.find("0x0000") != enums.end());
+}
+
+TEST_CASE("Enum illegal assignments")
+{
+    std::string test_idl_string_assignment =
+        "namespace Windows.Test { \
+            enum Alignment \
+            { \
+                Center = \"test\", \
+            } \
+        }";
+    
+    xlang_test_listener listener1;
+    REQUIRE(setup_and_run_parser(test_idl_string_assignment, listener1, true) == 1);
+
+    std::string test_idl_float_assignment =
+        "namespace Windows.Test { \
+            enum Alignment \
+            { \
+                Right = 1.9 \
+            } \
+        }";
+
+    xlang_test_listener listener2;
+    REQUIRE(setup_and_run_parser(test_idl_float_assignment, listener2, true) == 1);
 }
