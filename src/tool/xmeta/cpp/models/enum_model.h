9709740f56f83a3f4e33dfe0f70600a002a2f2d8
Tim Romanski <t-tiroma@microsoft.com>
2019-04-16 13:25:41 -0700

    Move implementations from cpp to headers
    Move implementations from cpp files to headers so that auto return types can resolve using just the header.

--- src/tool/xmeta/cpp/models/enum_model.h
+++ src/tool/xmeta/cpp/models/enum_model.h
@@ -7,6 +7,7 @@
 #include <variant>
 
 #include "base_model.h"
+#include "model_ref.h"
 
 namespace xlang::xmeta
 {
@@ -32,37 +33,96 @@ namespace xlang::xmeta
         int64_t,
         uint64_t>;
 
+
+
     struct enum_member
     {
         enum_member() = delete;
-        enum_member(std::string_view const& id, std::optional<enum_member_semantics> val);
-        enum_member(std::string_view const& id, std::string_view const& expression_id);
+        enum_member(std::string_view const& id, std::string_view const& str_val, enum_semantics type) :
+            m_id{ id },
+            m_value{ str_val }
+        {
+            assert(convert_value(type) == std::errc());
+        }
 
-        auto const& get_id() const noexcept;
-        auto const& get_value() const noexcept;
-        auto const& get_expression_id() const noexcept;
+        enum_member(std::string_view const& id, std::string_view const& str_val) :
+            m_id{ id },
+            m_value{ str_val }
+        { }
 
-        void set_value(enum_member_semantics const& val) noexcept;
+        auto const& get_id() const noexcept
+        {
+            return m_id;
+        }
+
+        auto const& get_value() const noexcept
+        {
+            return m_value;
+        }
+
+        std::errc convert_value(enum_semantics type) noexcept
+        {
+            std::string_view str_val{ m_value.get_ref_name() };
+            switch (type)
+            {
+            case enum_semantics::Int8:
+                return extract_str_val<int8_t>(str_val);
+            case enum_semantics::Uint8:
+                return extract_str_val<uint8_t>(str_val);
+            case enum_semantics::Int16:
+                return extract_str_val<int16_t>(str_val);
+            case enum_semantics::Uint16:
+                return extract_str_val<uint16_t>(str_val);
+            case enum_semantics::Int32:
+                return extract_str_val<int32_t>(str_val);
+            case enum_semantics::Uint32:
+                return extract_str_val<uint32_t>(str_val);
+            case enum_semantics::Int64:
+                return extract_str_val<int64_t>(str_val);
+            case enum_semantics::Uint64:
+                return extract_str_val<uint64_t>(str_val);
+            }
+        }
 
     private:
         std::string m_id;
-        std::optional<enum_member_semantics> m_value;
+        model_ref<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t> m_value;
 
-        // Temporarily stores ID referenced in an enum assignment.
-        std::string m_expression_id;
+        template<typename T>
+        std::errc extract_str_val(std::string_view const& str_val)
+        {
+            T result;
+            auto[p, ec] = std::from_chars(str_val.data(), str_val.data() + str_val.size(), result);
+            if (ec == std::errc())
+            {
+                m_value = result;
+            }
+            return ec;
+        }
     };
 
     struct enum_model : base_model
     {
         enum_model() = delete;
-        enum_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, enum_semantics t);
+        enum_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, enum_semantics t) :
+            base_model{ id, decl_line, assembly_name },
+            m_type{ t }
+        { }
 
-        auto const& get_members() const noexcept;
-        auto const& get_type() const noexcept;
+        auto const& get_members() const noexcept
+        {
+            return m_members;
+        }
 
-        std::errc assign_value(std::string_view const& member_id, std::string_view const& str_val) noexcept;
-        void add_member(std::string_view const& id, std::optional<enum_member_semantics> val);
-        void add_member(std::string_view const& id, std::string_view const& expression_id);
+        auto get_type() const noexcept
+        {
+            return m_type;
+        }
+
+        void add_member(std::string_view const& id, std::string_view const& str_val, bool is_numeric)
+        {
+            m_members.emplace_back(is_numeric ? enum_member{ id, str_val, m_type } : enum_member{ id, str_val });
+        }
 
     private:
         std::vector<enum_member> m_members;
