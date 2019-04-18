404b25e49e119f86d3b93a4049615a623a3858bd
Harry Pierson <hpierson@microsoft.com>
2019-04-17 17:51:41 -0700

    another step closer to building

--- src/package/pywinrt/projection/build.ps1
+++ src/package/pywinrt/projection/build.ps1
@@ -1,9 +1,27 @@
 
-$buildType = "Release"
 $repoRootPath = (get-item $PSScriptRoot).parent.Parent.parent.Parent.FullName
-$sourcePath = $PSScriptRoot.Replace('\', '/')
-$buildPath = "$repoRootPath/_build/_pywinrt_projection/$env:VSCMD_ARG_TGT_ARCH-$buildType".Replace('\', '/')
+$projectionPath = "$PSScriptRoot"
+$sourcePath = $projectionPath.Replace('\', '/')
+$pywinrt_path = "$sourcePath/pywinrt"
 
-# del $buildPath -rec -for
-cmake -S $sourcePath "-B$buildPath" -GNinja -DCMAKE_BUILD_TYPE=$buildType -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+$buildType = "Release"
+$buildPath = "$repoRootPath/_build/py-projection/$env:VSCMD_ARG_TGT_ARCH-$buildType".Replace('\', '/')
+# TODO: specify abi version number via param
+$pythonTag = "cp37"
+
+if ($env:VSCMD_ARG_TGT_ARCH -eq "x64")
+{
+    $platformTag = "win_amd64"
+}
+elseif ($env:VSCMD_ARG_TGT_ARCH -eq "x86")
+{
+    $platformTag = "win_x86"
+}
+else 
+{
+    Write-Error "VSCMD_ARG_TGT_ARCH not set to an expected value (x86 or x64)"
+    exit 
+}
+
+cmake -S $sourcePath "-B$buildPath" -GNinja "-DPYTHON_TAG=$pythonTag" "-DPLATFORM_TAG=$platformTag" -DCMAKE_BUILD_TYPE=$buildType -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
 cmake --build $buildPath -- -v 
--- src/package/pywinrt/projection/configure.ps1
+++ src/package/pywinrt/projection/configure.ps1
@@ -1,5 +1,15 @@
+param ([switch]$clean)
+
 $windows_sdk = '10.0.17763.0'
 $repoRootPath = (get-item $PSScriptRoot).parent.Parent.parent.Parent.FullName
+$projectionPath = "$PSScriptRoot"
+
+if ($clean)
+{
+    write-host "removing generated projection files"
+    remove-item $projectionPath -rec -for -ErrorAction SilentlyContinue
+    exit
+}
 
 $pywinrt_exe = Get-ChildItem $repoRootPath\_build\Windows\*\*\tool\python\pywinrt.exe | 
     Sort-Object -Descending | Select-Object -first 1
@@ -9,25 +19,40 @@ if (-not $pywinrt_exe) {
     exit
 }
 
-nuget install Microsoft.Windows.CppWinRT -ExcludeVersion -OutputDirectory $repoRootPath/_build/_tools
-nuget install python -ExcludeVersion -OutputDirectory $PSScriptRoot
-nuget install pythonx86 -ExcludeVersion -OutputDirectory $PSScriptRoot
+nuget install Microsoft.Windows.CppWinRT -ExcludeVersion -OutputDirectory $projectionPath
 
-$cppwinrt_exe = "$repoRootPath/_build/_tools/Microsoft.Windows.CppWinRT\bin\cppwinrt.exe"
+# TODO: pass python version as param
+$pythonVersion = "3.7.3"
 
-& $cppwinrt_exe -in $windows_sdk -out $PSScriptRoot/cppwinrt -verbose
+if ($env:VSCMD_ARG_TGT_ARCH -eq "x64")
+{
+    nuget install python -ExcludeVersion -OutputDirectory $projectionPath -version $pythonVersion
+}
+elseif ($env:VSCMD_ARG_TGT_ARCH -eq "x86")
+{
+    nuget install pythonx86 -ExcludeVersion -OutputDirectory $projectionPath -version $pythonVersion
+}
+else 
+{
+    Write-Error "VSCMD_ARG_TGT_ARCH not set to an expected value (x86 or x64)"
+    exit 
+}
 
-$pywinrt_path = "$PSScriptRoot/pywinrt"
+$cppwinrt_exe = "$projectionPath/Microsoft.Windows.CppWinRT\bin\cppwinrt.exe"
+
+& $cppwinrt_exe -input $windows_sdk -output $projectionPath/cppwinrt -verbose
+
+$pywinrt_path = "$projectionPath/pywinrt"
 
 remove-item $pywinrt_path -Recurse -Force -ErrorAction SilentlyContinue
 
-#$pyinclude = "Windows.Data.Json", "Windows.Devices.Geolocation", "Windows.Foundation", "Windows.Graphics.DirectX"
-$pyinclude = @("Windows.")
+$pyinclude = "Windows.Data.Json", "Windows.Devices.Geolocation", "Windows.Foundation", "Windows.Graphics.DirectX"
+#$pyinclude = @("Windows.")
 #$pyexclude = "Windows.UI.Comp", "Windows.UI.Xaml"
 $pyexclude = "Windows.UI.Composition", "Windows.UI.Xaml"
 $pyin = $pyinclude | ForEach-Object{ "-include", "$_"}
 $pyout = $pyexclude | ForEach-Object{ "-exclude", "$_"}
 
-$pyparams = ("-in", $windows_sdk, "-out", $pywinrt_path, "-verbose") + $pyin + $pyout
+$pyparams = ("-input", $windows_sdk, "-output", $pywinrt_path, "-verbose") + $pyin + $pyout
 
 & $pywinrt_exe $pyparams
