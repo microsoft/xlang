90e15b0de6c36c5a3c8cc5a2655d516265acb46f
Harry Pierson <hpierson@microsoft.com>
2019-04-19 09:07:26 -0700

    fix setup.py and rever whole projection change for testing

--- src/tool/python/strings/setup.py
+++ src/tool/python/strings/setup.py
@@ -6,5 +6,5 @@ setuptools.setup(
     description="Generated Python/WinRT package",
     license="MIT",
     url="http://github.com/Microsoft/xlang",
-    package_data={ "%":["_%.*.pyd"] },
+    package_data={ "%":["_%.pyd"] },
     packages = setuptools.find_namespace_packages(where='.', include=("%*")))
