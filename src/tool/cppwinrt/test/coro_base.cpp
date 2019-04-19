e8092a84d54eb7a4632408389c601b332f2b3b11
Kenny Kerr <kekerr@microsoft.com>
2019-04-19 10:53:36 -0700

    testing

--- src/tool/cppwinrt/test/coro_base.cpp
+++
@@ -1,9 +0,0 @@
-#include "winrt/base.h"
-#include "catch.hpp"
-
-using winrt;
-
-TEST_CASE("coro_base")
-{
-
-}
---
+++ src/tool/cppwinrt/test/coro_threadpool.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+// Only need winrt/base.h for coroutine thread pool support.
+#include "winrt/base.h"
+
+using namespace winrt;
+
+namespace
+{
+    fire_and_forget Async()
+    {
+        co_await resume_background();
+    }
+}
+
+TEST_CASE("coro_base")
+{
+    Async();
+}
