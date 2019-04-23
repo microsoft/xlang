c4aad909ed843be62cf48a909094b456ad224139
Will Thant <kythant@microsoft.com>
2019-04-23 00:57:20 -0700

    main

--- src/tool/xmeta/main.cpp
+++ src/tool/xmeta/main.cpp
@@ -6,6 +6,8 @@
 
 #include "xmeta_idl_reader.h"
 #include "ast_to_st_listener.h"
+#include "xlang_model_walker.h"
+#include "xmeta_emit.h"
 
 #if defined(_WIN32)
 #include <Windows.h>
@@ -18,8 +20,33 @@
 
 using namespace antlr4;
 using namespace winrt;
+using namespace xlang::xmeta;
+
+std::string remove_extension(const std::string& filename) {
+    size_t lastdot = filename.find_last_of(".");
+    if (lastdot == std::string::npos) return filename;
+    return filename.substr(0, lastdot);
+}
+
+std::string remove_path(const std::string& filename) {
+    size_t lastdot = filename.find_last_of("\\");
+    if (lastdot == std::string::npos) return filename;
+    return filename.substr(lastdot + 1, filename.length());
+}
 
 int main(int argc, const char* argv[])
 {
-    return 0;
+    std::ifstream stream;
+    stream.open(argv[1]);
+    xmeta_idl_reader reader{ "" };
+    reader.read(stream);
+
+    std::string assembly_name = remove_path(remove_extension(std::string(argv[1])));
+    xlang_model_walker walker(reader.get_namespaces());
+    std::shared_ptr<xmeta_emit> emitter = std::make_shared<xmeta_emit>(assembly_name);
+
+    emitter->initialize();
+    walker.register_listener(emitter);
+    walker.walk();
+    emitter->save_to_file();
 }
