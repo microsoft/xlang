29a1a023ff1af6e5762545a5e488667e1adc9a2c
Tim Romanski <t-tiroma@microsoft.com>
2019-04-22 17:18:28 -0700

    Fix method_model bug
    construct m_implemented_method_ref in method_model ctor

--- src/tool/xmeta/cpp/models/method_model.h
+++ src/tool/xmeta/cpp/models/method_model.h
@@ -6,6 +6,7 @@
 
 #include "base_model.h"
 #include "formal_parameter_model.h"
+#include "model_ref.h"
 #include "model_types.h"
 
 namespace xlang::xmeta
@@ -23,7 +24,8 @@ namespace xlang::xmeta
             base_model{ id, decl_line, assembly_name },
             m_formal_parameters{ std::move(formal_params) },
             m_return_type{ std::move(return_type) },
-            m_semantic{ sem }
+            m_semantic{ sem },
+            m_implemented_method_ref{ "" }
         { }
 
         method_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, std::optional<type_ref>&& return_type, std::vector<formal_parameter_model>&& formal_params, std::string_view const& overridden_method_ref) :
@@ -55,7 +57,7 @@ namespace xlang::xmeta
 
         void set_overridden_method_ref(std::shared_ptr<method_model> const& ref) noexcept
         {
-            m_implemented_method_ref = ref;
+            m_implemented_method_ref.resolve(ref);
         }
 
     private:
