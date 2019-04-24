e72a635fd1ce6fe4e5419bb5ad659424bdfb335b
Kenny Kerr <kekerr@microsoft.com>
2019-04-24 15:19:02 -0700

    Fix bug in -optimize colliding with winrt::param types (#352)

--- src/tool/cppxlang/strings/base_collections_input_vector.h
+++ src/tool/cppxlang/strings/base_collections_input_vector.h
@@ -73,6 +73,11 @@ namespace xlang::param
             }
         }
 
+        operator interface_type const& () const noexcept
+        {
+            return m_interface;
+        }
+
     private:
 
         interface_type m_interface;
