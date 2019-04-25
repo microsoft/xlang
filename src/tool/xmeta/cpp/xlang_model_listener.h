bf13e135f91d4c09b032e5ffd2a02a33fcd369c8
Will Thant <kythant@microsoft.com>
2019-04-25 14:07:07 -0700

    Modified xlang model listener

--- src/tool/xmeta/cpp/xlang_model_listener.h
+++ src/tool/xmeta/cpp/xlang_model_listener.h
@@ -4,14 +4,13 @@
 
 namespace xlang::xmeta
 {
-    class xlang_model_listener
+    struct xlang_model_listener
     {
-    public:
-        virtual void listen_namespace_model(std::shared_ptr<namespace_model> const& model) = 0;
-        virtual void listen_class_model(std::shared_ptr<class_model> const& model) = 0;
-        virtual void listen_struct_model(std::shared_ptr<struct_model> const& model) = 0;
-        virtual void listen_interface_model(std::shared_ptr<interface_model> const& model) = 0;
-        virtual void listen_enum_model(std::shared_ptr<enum_model> const& model) = 0;
-        virtual void listen_delegate_model(delegate_model const& model) = 0;
+        virtual void listen_namespace_model(std::shared_ptr<namespace_model> const& model) {};
+        virtual void listen_class_model(std::shared_ptr<class_model> const& model) {};
+        virtual void listen_struct_model(std::shared_ptr<struct_model> const& model) {};
+        virtual void listen_interface_model(std::shared_ptr<interface_model> const& model) {};
+        virtual void listen_enum_model(std::shared_ptr<enum_model> const& model) {};
+        virtual void listen_delegate_model(delegate_model const& model) {};
     };
 }
