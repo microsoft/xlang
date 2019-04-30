e8e2bbd0a3c83d7b4536953b6756589b24a9567e
Kenny Kerr <kekerr@microsoft.com>
2019-04-30 16:18:41 -0700

    More efficient GUID comparisons in C++/WinRT (#372)

--- src/tool/cppwinrt/strings/base_types.h
+++ src/tool/cppwinrt/strings/base_types.h
@@ -17,11 +17,6 @@ namespace winrt::impl
     using srwlock = struct srwlock_*;
     using condition_variable = struct condition_variable_*;
     using bstr = wchar_t*;
-
-    inline bool is_guid_equal(uint32_t const* const left, uint32_t const* const right) noexcept
-    {
-        return left[0] == right[0] && left[1] == right[1] && left[2] == right[2] && left[3] == right[3];
-    }
 }
 
 namespace winrt
@@ -80,7 +75,7 @@ namespace winrt
 
     inline bool operator==(guid const& left, guid const& right) noexcept
     {
-        return impl::is_guid_equal(reinterpret_cast<uint32_t const*>(&left), reinterpret_cast<uint32_t const*>(&right));
+        return !memcmp(&left, &right, sizeof(guid));
     }
 
     inline bool operator!=(guid const& left, guid const& right) noexcept
