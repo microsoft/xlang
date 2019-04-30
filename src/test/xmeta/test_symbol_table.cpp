90cef2e33fe099f841fc0eb001491ceb702e6df5
kythant <kythant@microsoft.com>
2019-04-29 23:42:40 -0700

    Delegate population

--- src/test/xmeta/test_symbol_table.cpp
+++ src/test/xmeta/test_symbol_table.cpp
@@ -15,29 +15,29 @@ using namespace xlang::xmeta;
 
 TEST_CASE("Duplicate Namespaces")
 {
-    std::istringstream test_idl(R"(
-        namespace n { }
-        namespace n { }
+	std::istringstream test_idl(R"(
+        namespace N { }
+        namespace N { }
     )");
 
-    xmeta_idl_reader reader{ "" };
-    REQUIRE(reader.read(test_idl) == 0);
+	xmeta_idl_reader reader{ "" };
+	REQUIRE(reader.read(test_idl) == 0);
 
-    auto namespaces = reader.get_namespaces();
-    auto it = namespaces.find("n");
-    REQUIRE(it != namespaces.end());
-    auto ns = it->second;
-    REQUIRE(ns->get_namespace_bodies().size() == 2);
-    REQUIRE(ns->get_namespace_bodies()[0]->get_containing_namespace() == ns);
-    REQUIRE(ns->get_namespace_bodies()[1]->get_containing_namespace() == ns);
+	auto namespaces = reader.get_namespaces();
+	auto it = namespaces.find("N");
+	REQUIRE(it != namespaces.end());
+	auto ns = it->second;
+	REQUIRE(ns->get_namespace_bodies().size() == 2);
+	REQUIRE(ns->get_namespace_bodies()[0]->get_containing_namespace() == ns);
+	REQUIRE(ns->get_namespace_bodies()[1]->get_containing_namespace() == ns);
 }
 
 TEST_CASE("Enum test")
 {
-    std::istringstream test_idl{ R"(
-        namespace n
+	std::istringstream test_idl{ R"(
+        namespace N
         {
-            enum e
+            enum E
             {
                 e_member_1,
                 e_member_2 = 3,
@@ -48,43 +48,43 @@ TEST_CASE("Enum test")
         }
     )" };
 
-    xmeta_idl_reader reader{ "" };
-    REQUIRE(reader.read(test_idl) == 0);
+	xmeta_idl_reader reader{ "" };
+	REQUIRE(reader.read(test_idl) == 0);
 
-    auto namespaces = reader.get_namespaces();
-    auto it = namespaces.find("n");
-    REQUIRE(it != namespaces.end());
-    auto ns = it->second;
-    REQUIRE(ns->get_namespace_bodies().size() == 1);
-    auto ns_body = ns->get_namespace_bodies()[0];
+	auto namespaces = reader.get_namespaces();
+	auto it = namespaces.find("N");
+	REQUIRE(it != namespaces.end());
+	auto ns = it->second;
+	REQUIRE(ns->get_namespace_bodies().size() == 1);
+	auto ns_body = ns->get_namespace_bodies()[0];
 
-    auto enums = ns_body->get_enums();
-    REQUIRE(enums.find("e") != enums.end());
-    auto const& enum_members = enums["e"]->get_members();
-    REQUIRE(enum_members.size() == 5);
-    REQUIRE(enum_members[0].get_id() == "e_member_1");
-    REQUIRE(enum_members[1].get_id() == "e_member_2");
-    REQUIRE(enum_members[2].get_id() == "e_member_3");
-    REQUIRE(enum_members[3].get_id() == "e_member_4");
-    REQUIRE(enum_members[4].get_id() == "e_member_5");
-    auto const& val1 = enum_members[0].get_value();
-    auto const& val2 = enum_members[1].get_value();
-    auto const& val3 = enum_members[2].get_value();
-    auto const& val4 = enum_members[3].get_value();
-    auto const& val5 = enum_members[4].get_value();
-    REQUIRE((val1.is_resolved() && std::get<int32_t>(val1.get_resolved_target()) == 0));
-    REQUIRE((val2.is_resolved() && std::get<int32_t>(val2.get_resolved_target()) == 3));
-    REQUIRE((val3.is_resolved() && std::get<int32_t>(val3.get_resolved_target()) == 4));
-    REQUIRE((val4.is_resolved() && std::get<int32_t>(val4.get_resolved_target()) == 0x21));
-    REQUIRE((val5.is_resolved() && std::get<int32_t>(val5.get_resolved_target()) == 0x21));
+	auto enums = ns_body->get_enums();
+	REQUIRE(enums.find("E") != enums.end());
+	auto const& enum_members = enums["E"]->get_members();
+	REQUIRE(enum_members.size() == 5);
+	REQUIRE(enum_members[0].get_id() == "e_member_1");
+	REQUIRE(enum_members[1].get_id() == "e_member_2");
+	REQUIRE(enum_members[2].get_id() == "e_member_3");
+	REQUIRE(enum_members[3].get_id() == "e_member_4");
+	REQUIRE(enum_members[4].get_id() == "e_member_5");
+	auto const& val1 = enum_members[0].get_value();
+	auto const& val2 = enum_members[1].get_value();
+	auto const& val3 = enum_members[2].get_value();
+	auto const& val4 = enum_members[3].get_value();
+	auto const& val5 = enum_members[4].get_value();
+	REQUIRE((val1.is_resolved() && std::get<int32_t>(val1.get_resolved_target()) == 0));
+	REQUIRE((val2.is_resolved() && std::get<int32_t>(val2.get_resolved_target()) == 3));
+	REQUIRE((val3.is_resolved() && std::get<int32_t>(val3.get_resolved_target()) == 4));
+	REQUIRE((val4.is_resolved() && std::get<int32_t>(val4.get_resolved_target()) == 0x21));
+	REQUIRE((val5.is_resolved() && std::get<int32_t>(val5.get_resolved_target()) == 0x21));
 }
 
 TEST_CASE("Enum circular dependency")
 {
-    std::istringstream implicit_dependency_error_idl{ R"(
-        namespace n
+	std::istringstream implicit_dependency_error_idl{ R"(
+        namespace N
         {
-            enum e
+            enum E
             {
                 e_member_1 = e_member_3,
                 e_member_2,
@@ -92,10 +92,10 @@ TEST_CASE("Enum circular dependency")
             }
         }
     )" };
-    std::istringstream explicit_dependency_error_idl{ R"(
-        namespace n
+	std::istringstream explicit_dependency_error_idl{ R"(
+        namespace N
         {
-            enum e
+            enum E
             {
                 e_member_1 = e_member_2,
                 e_member_2 = e_member_1
@@ -103,10 +103,54 @@ TEST_CASE("Enum circular dependency")
         }
     )" };
 
-    xmeta_idl_reader reader{ "" };
-    REQUIRE(reader.read(implicit_dependency_error_idl) == 0);
-    REQUIRE(reader.get_num_semantic_errors() == 1);
-    reader.reset("");
-    REQUIRE(reader.read(explicit_dependency_error_idl) == 0);
-    REQUIRE(reader.get_num_semantic_errors() == 1);
+	xmeta_idl_reader reader{ "" };
+	REQUIRE(reader.read(implicit_dependency_error_idl) == 0);
+	REQUIRE(reader.get_num_semantic_errors() == 1);
+	reader.reset("");
+	REQUIRE(reader.read(explicit_dependency_error_idl) == 0);
+	REQUIRE(reader.get_num_semantic_errors() == 1);
+}
+
+TEST_CASE("Delegate test")
+{
+	std::istringstream test_idl{ R"(
+        namespace N
+        {
+            enum E { }
+            enum F{}
+            delegate Int32 D1(Int32 i, Double d, E e);
+            delegate void D2();
+        }
+    )" };
+
+	xmeta_idl_reader reader{ "" };
+	REQUIRE(reader.read(test_idl) == 0);
+
+	auto namespaces = reader.get_namespaces();
+	auto it = namespaces.find("N");
+	REQUIRE(it != namespaces.end());
+	auto ns_bodies = it->second->get_namespace_bodies();
+	REQUIRE(ns_bodies.size() == 1);
+	auto delegates = ns_bodies[0]->get_delegates();
+	REQUIRE(delegates.size() == 2);
+
+	REQUIRE(delegates.find("D1") != delegates.end());
+	REQUIRE(delegates.at("D1")->get_return_type() != std::nullopt);
+	auto del0_return_type = delegates.at("D1")->get_return_type()->get_semantic();
+	REQUIRE((del0_return_type.is_resolved() && std::get<simple_type>(del0_return_type.get_resolved_target()) == simple_type::Int32));
+	auto del0_formal_params = delegates.at("D1")->get_formal_parameters();
+	REQUIRE(del0_formal_params.size() == 3);
+	auto del0_formal_param_0_type = del0_formal_params[0].get_type().get_semantic();
+	auto del0_formal_param_1_type = del0_formal_params[1].get_type().get_semantic();
+	auto del0_formal_param_2_type = del0_formal_params[2].get_type().get_semantic();
+	REQUIRE(del0_formal_params[0].get_id() == "i");
+	REQUIRE(del0_formal_params[1].get_id() == "d");
+	REQUIRE(del0_formal_params[2].get_id() == "e");
+	REQUIRE((del0_formal_param_0_type.is_resolved() && std::get<simple_type>(del0_formal_param_0_type.get_resolved_target()) == simple_type::Int32));
+	REQUIRE((del0_formal_param_1_type.is_resolved() && std::get<simple_type>(del0_formal_param_1_type.get_resolved_target()) == simple_type::Double));
+	REQUIRE((!del0_formal_param_2_type.is_resolved() && del0_formal_param_2_type.get_ref_name() == "E"));
+
+	REQUIRE(delegates.find("D2") != delegates.end());
+	REQUIRE(delegates.at("D2")->get_return_type() == std::nullopt);
+	REQUIRE(delegates.at("D2")->get_formal_parameters().empty());
 }
--- src/tool/xmeta/cpp/models/using_directive_models.h
+++ src/tool/xmeta/cpp/models/using_directive_models.h
@@ -10,68 +10,79 @@
 
 namespace xlang::xmeta
 {
-    struct namespace_model;
+	struct namespace_model;
 
-    struct using_alias_directive_model : base_model
-    {
-        using_alias_directive_model() = delete;
-        using_alias_directive_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, std::string_view const& type_or_ns_name) :
-            base_model{ id, decl_line, assembly_name },
-            m_type_or_namespace_ref{ type_or_ns_name }
-        { }
+	struct using_alias_directive_model : base_model
+	{
+		using_alias_directive_model() = delete;
+		using_alias_directive_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, std::string_view const& type_or_ns_name) :
+			base_model{ id, decl_line, assembly_name },
+			m_type_or_namespace_ref{ type_or_ns_name }
+		{ }
 
-        bool is_namespace_ref() const noexcept
-        {
-            return m_type_or_namespace_ref.is_resolved() && 
-                std::holds_alternative<std::shared_ptr<namespace_model>>(m_type_or_namespace_ref.get_resolved_target());
-        }
+		auto const& get_type_or_namespace_ref() const noexcept
+		{
+			return m_type_or_namespace_ref;
+		}
 
-        bool is_type_ref() const noexcept
-        {
-            return m_type_or_namespace_ref.is_resolved() &&
-                std::holds_alternative<type_semantics>(m_type_or_namespace_ref.get_resolved_target());
-        }
+		void set_namespace_ref(std::shared_ptr<class_model> const& nm) noexcept
+		{
+			assert(m_type_or_namespace_ref.holds_type<std::shared_ptr<class_model>>());
+			m_type_or_namespace_ref.resolve(nm);
+		}
 
-        auto const& get_type_or_namespace_ref() const noexcept
-        {
-            return m_type_or_namespace_ref;
-        }
+		void set_namespace_ref(std::shared_ptr<enum_model> const& nm) noexcept
+		{
+			assert(m_type_or_namespace_ref.holds_type<std::shared_ptr<enum_model>>());
+			m_type_or_namespace_ref.resolve(nm);
+		}
 
-        void set_type_ref(type_semantics const& sem) noexcept
-        {
-            assert(is_type_ref());
-            m_type_or_namespace_ref.resolve(sem);
-        }
+		void set_namespace_ref(std::shared_ptr<interface_model> const& nm) noexcept
+		{
+			assert(m_type_or_namespace_ref.holds_type<std::shared_ptr<interface_model>>());
+			m_type_or_namespace_ref.resolve(nm);
+		}
 
-        void set_namespace_ref(std::shared_ptr<namespace_model> const& nm) noexcept
-        {
-            assert(is_namespace_ref());
-            m_type_or_namespace_ref.resolve(nm);
-        }
+		void set_namespace_ref(std::shared_ptr<struct_model> const& nm) noexcept
+		{
+			assert(m_type_or_namespace_ref.holds_type<std::shared_ptr<struct_model>>());
+			m_type_or_namespace_ref.resolve(nm);
+		}
 
-    private:
-        model_ref<type_semantics, std::shared_ptr<namespace_model>> m_type_or_namespace_ref;
-    };
+		void set_namespace_ref(std::shared_ptr<namespace_model> const& nm) noexcept
+		{
+			assert(m_type_or_namespace_ref.holds_type<std::shared_ptr<namespace_model>>());
+			m_type_or_namespace_ref.resolve(nm);
+		}
 
-    struct using_namespace_directive_model : base_model
-    {
-        using_namespace_directive_model() = delete;
-        using_namespace_directive_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, std::string_view const& ns_name) :
-            base_model{ id, decl_line, assembly_name },
-            m_namespace_ref{ ns_name }
-        { }
+	private:
+		model_ref<
+			std::shared_ptr<class_model>,
+			std::shared_ptr<enum_model>,
+			std::shared_ptr<interface_model>,
+			std::shared_ptr<struct_model>,
+			std::shared_ptr<namespace_model>> m_type_or_namespace_ref;
+	};
 
-        auto const& get_namespace_ref() noexcept
-        {
-            return m_namespace_ref;
-        }
+	struct using_namespace_directive_model : base_model
+	{
+		using_namespace_directive_model() = delete;
+		using_namespace_directive_model(std::string_view const& id, size_t decl_line, std::string_view const& assembly_name, std::string_view const& ns_name) :
+			base_model{ id, decl_line, assembly_name },
+			m_namespace_ref{ ns_name }
+		{ }
 
-        void set_namespace_ref(std::shared_ptr<namespace_model> const& nm) noexcept
-        {
-            m_namespace_ref.resolve(nm);
-        }
+		auto const& get_namespace_ref() noexcept
+		{
+			return m_namespace_ref;
+		}
 
-    private:
-        model_ref<std::shared_ptr<namespace_model>> m_namespace_ref;
-    };
+		void set_namespace_ref(std::shared_ptr<namespace_model> const& nm) noexcept
+		{
+			m_namespace_ref.resolve(nm);
+		}
+
+	private:
+		model_ref<std::shared_ptr<namespace_model>> m_namespace_ref;
+	};
 }
