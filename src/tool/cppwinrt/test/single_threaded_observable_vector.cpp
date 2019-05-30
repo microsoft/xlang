374ab8bb2ef9c5c48a1179f54e12db0e77149f60
Kenny Kerr <kekerr@microsoft.com>
2019-05-30 09:26:22 -0700

    test

--- src/tool/cppwinrt/test/single_threaded_observable_vector.cpp
+++ src/tool/cppwinrt/test/single_threaded_observable_vector.cpp
@@ -56,4 +56,104 @@ TEST_CASE("single_threaded_observable_vector")
         REQUIRE(changed_i);
         REQUIRE(changed_o);
     }
+    {
+        IObservableVector<int> vector_i = single_threaded_observable_vector<int>();
+        IObservableVector<IInspectable> vector_o = vector_i.as<IObservableVector<IInspectable>>();
+
+        vector_i.Append(0);
+        vector_o.Append(box_value(1));
+
+        REQUIRE(vector_i.Size() == 2);
+        REQUIRE(vector_o.Size() == 2);
+
+        REQUIRE(vector_i.GetAt(0) == 0);
+        REQUIRE(unbox_value<int>(vector_o.GetAt(0)) == 0);
+
+        REQUIRE(vector_i.GetAt(1) == 1);
+        REQUIRE(unbox_value<int>(vector_o.GetAt(1)) == 1);
+    }
+    {
+        IObservableVector<int> vector_i = single_threaded_observable_vector<int>({ 1,2 });
+        IObservableVector<IInspectable> vector_o = vector_i.as<IObservableVector<IInspectable>>();
+
+        IIterator<int> iterator_i = vector_i.First();
+        IIterator<IInspectable> iterator_o = vector_o.First();
+
+        REQUIRE(iterator_i.HasCurrent());
+        REQUIRE(iterator_o.HasCurrent());
+
+        REQUIRE(iterator_i.Current() == 1);
+        REQUIRE(unbox_value<int>(iterator_o.Current()) == 1);
+
+        REQUIRE(iterator_i.MoveNext());
+        REQUIRE(iterator_o.MoveNext());
+
+        REQUIRE(iterator_i.HasCurrent());
+        REQUIRE(iterator_o.HasCurrent());
+
+        REQUIRE(iterator_i.Current() == 2);
+        REQUIRE(unbox_value<int>(iterator_o.Current()) == 2);
+
+        REQUIRE(!iterator_i.MoveNext());
+        REQUIRE(!iterator_o.MoveNext());
+
+        REQUIRE(!iterator_i.HasCurrent());
+        REQUIRE(!iterator_o.HasCurrent());
+
+        REQUIRE_THROWS_AS(iterator_i.Current(), hresult_out_of_bounds);
+        REQUIRE_THROWS_AS(iterator_o.Current(), hresult_out_of_bounds);
+    }
+    {
+        IObservableVector<int> vector_i = single_threaded_observable_vector<int>({ 1,2 });
+        IObservableVector<IInspectable> vector_o = vector_i.as<IObservableVector<IInspectable>>();
+
+        IIterator<int> iterator_i = vector_i.First();
+        IIterator<IInspectable> iterator_o = vector_o.First();
+
+        std::array<int, 3> array_i{};
+        REQUIRE(2 == iterator_i.GetMany(array_i));
+        REQUIRE(array_i[0] == 1);
+        REQUIRE(array_i[1] == 2);
+        REQUIRE(array_i[2] == 0);
+
+        std::array<IInspectable, 3> array_o{};
+        REQUIRE(2 == iterator_o.GetMany(array_o));
+        REQUIRE(unbox_value<int>(array_o[0]) == 1);
+        REQUIRE(unbox_value<int>(array_o[1]) == 2);
+        REQUIRE(array_o[2] == nullptr);
+
+        REQUIRE(0 == iterator_i.GetMany(array_i));
+        REQUIRE(0 == iterator_o.GetMany(array_o));
+    }
+    {
+        IObservableVector<int> vector_i = single_threaded_observable_vector<int>({ 1,2 });
+        IObservableVector<IInspectable> vector_o = vector_i.as<IObservableVector<IInspectable>>();
+
+        IIterator<int> iterator_i = vector_i.First();
+        IIterator<IInspectable> iterator_o = vector_o.First();
+
+        std::array<int, 1> array_i{};
+        REQUIRE(1 == iterator_i.GetMany(array_i));
+        REQUIRE(array_i[0] == 1);
+
+        std::array<IInspectable, 1> array_o{};
+        REQUIRE(1 == iterator_o.GetMany(array_o));
+        REQUIRE(unbox_value<int>(array_o[0]) == 1);
+    }
+
+    {
+        IObservableVector<int> vector_i = single_threaded_observable_vector<int>({ 1,2 });
+        IObservableVector<IInspectable> vector_o = vector_i.as<IObservableVector<IInspectable>>();
+
+        IIterator<int> iterator_i = vector_i.First();
+        IIterator<IInspectable> iterator_o = vector_o.First();
+
+        iterator_i.HasCurrent();
+        iterator_o.HasCurrent();
+
+        vector_i.Append(3);
+
+        REQUIRE_THROWS_AS(iterator_i.HasCurrent(), hresult_changed_state);
+        REQUIRE_THROWS_AS(iterator_o.HasCurrent(), hresult_changed_state);
+    }
 }
