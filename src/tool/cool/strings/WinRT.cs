9b8a7b7baaeda8523ec7ea334bcd58892637aa23
David Fields (ANALOG) <dfields@microsoft.com>
2019-06-21 12:38:58 -0700

    Add workaround for Mono thread tainting to avoid deadlocks when cycling the Unity Editor

--- src/tool/cool/strings/WinRT.cs
+++ src/tool/cool/strings/WinRT.cs
@@ -204,6 +204,116 @@ namespace WinRT
         public static extern int WindowsDeleteString(IntPtr hstring);
     }
 
+    internal class Mono
+    {
+        static Lazy<bool> _usingMono = new Lazy<bool>(() =>
+        {
+            var modulePtr = Platform.LoadLibraryExW("mono-2.0-bdwgc.dll", IntPtr.Zero, 0);
+            if (modulePtr == IntPtr.Zero) return false;
+
+            if (!Platform.FreeLibrary(modulePtr))
+            {
+                Marshal.ThrowExceptionForHR(Marshal.GetHRForLastWin32Error());
+            }
+            return true;
+        });
+
+        [DllImport("mono-2.0-bdwgc.dll")]
+        static extern IntPtr mono_thread_current();
+
+        [DllImport("mono-2.0-bdwgc.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool mono_thread_is_foreign(IntPtr threadPtr);
+
+        [DllImport("mono-2.0-bdwgc.dll")]
+        static extern void mono_unity_thread_fast_attach(IntPtr domainPtr);
+
+        [DllImport("mono-2.0-bdwgc.dll")]
+        static extern void mono_unity_thread_fast_detach();
+
+        [DllImport("mono-2.0-bdwgc.dll")]
+        static extern void mono_thread_pop_appdomain_ref();
+
+        [DllImport("mono-2.0-bdwgc.dll")]
+        static extern IntPtr mono_domain_get();
+
+        struct MonoObject
+        {
+            IntPtr vtable;
+            IntPtr synchronisation; // sic
+        }
+
+        unsafe struct MonoThread
+        {
+            MonoObject obj;
+            public MonoInternalThread_x64* internal_thread;
+            IntPtr start_obj;
+            IntPtr pending_exception;
+        }
+
+        [Flags]
+        enum MonoThreadFlag : int
+        {
+            MONO_THREAD_FLAG_DONT_MANAGE = 1,
+            MONO_THREAD_FLAG_NAME_SET = 2,
+            MONO_THREAD_FLAG_APPDOMAIN_ABORT = 4,
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct MonoInternalThread_x64
+        {
+            [FieldOffset(0xd0)]
+            public MonoThreadFlag flags;
+        }
+
+        public class ThreadContext : IDisposable
+        {
+            static Lazy<HashSet<IntPtr>> _foreignThreads = new Lazy<HashSet<IntPtr>>();
+
+            readonly IntPtr _threadPtr = IntPtr.Zero;
+
+            public ThreadContext()
+            {
+                if (_usingMono.Value)
+                {
+                    // nothing to do for Mono-native threads
+                    var threadPtr = mono_thread_current();
+                    if (mono_thread_is_foreign(threadPtr))
+                    {
+                        // initialize this thread the first time it runs managed code, and remember it for future reference
+                        if (_foreignThreads.Value.Add(threadPtr))
+                        {
+                            // clear initial appdomain ref for new foreign threads to avoid deadlock on domain unload
+                            mono_thread_pop_appdomain_ref();
+
+                            unsafe
+                            {
+                                // tell Mono to ignore the thread on process shutdown since there's nothing to synchronize with
+                                ((MonoThread*)threadPtr)->internal_thread->flags |= MonoThreadFlag.MONO_THREAD_FLAG_DONT_MANAGE;
+                            }
+                        }
+
+                        unsafe
+                        {
+                            // attach as Unity does to set up the proper domain for the call
+                            mono_unity_thread_fast_attach(mono_domain_get());
+                            _threadPtr = threadPtr;
+                        }
+                    }
+                }
+            }
+
+            public void Dispose()
+            {
+                if (_threadPtr != IntPtr.Zero)
+                {
+                    // detach as Unity does to properly reset the domain context
+                    mono_unity_thread_fast_detach();
+                }
+            }
+        }
+    }
+
     internal class HString : ICloneable, IDisposable
     {
         public readonly IntPtr Handle;
@@ -512,12 +622,15 @@ namespace WinRT
         {
             try
             {
-                var invoker = (T)FindObject(thisPtr)._weakInvoker.Target;
-                if (invoker != null)
+                using (new Mono.ThreadContext())
                 {
-                    invoke(invoker);
+                    var invoker = (T)FindObject(thisPtr)._weakInvoker.Target;
+                    if (invoker != null)
+                    {
+                        invoke(invoker);
+                    }
+                    return 0; // S_OK;
                 }
-                return 0; // S_OK;
             }
             catch (Exception e)
             {
