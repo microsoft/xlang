e8841bcdc8880238dd0413961c08f8bb60552c15
Kenny Kerr <kekerr@microsoft.com>
2019-07-16 15:26:25 -0700

    test

--- src/tool/cppwinrt/test/when.cpp
+++ src/tool/cppwinrt/test/when.cpp
@@ -17,13 +17,52 @@ IAsyncAction async(bool& done)
     done = true;
 }
 
-TEST_CASE("when_all")
+IAsyncAction when_signaled(handle const& event)
 {
-    bool ppl_done = false;
-    bool async_done = false;
+    co_await resume_on_signal(event.get());
+}
 
-    winrt::when_all(ppl(ppl_done), async(async_done)).get();
+IAsyncAction done()
+{
+    co_return;
+}
 
-    REQUIRE(ppl_done);
-    REQUIRE(async_done);
+IAsyncOperation<int> done_int()
+{
+    co_return 0;
+}
+
+TEST_CASE("when")
+{
+    {
+        bool ppl_done = false;
+        bool async_done = false;
+
+        // Ensures that different async types can be aggregated.
+        winrt::when_all(ppl(ppl_done), async(async_done)).get();
+
+        REQUIRE(ppl_done);
+        REQUIRE(async_done);
+    }
+    {
+        // Ensures that different WinRT async types can be aggregated.
+        winrt::when_any(done(), done_int()).get();
+
+        handle first_event{ check_pointer(CreateEventW(nullptr, true, false, nullptr)) };
+        handle second_event{ check_pointer(CreateEventW(nullptr, true, false, nullptr)) };
+
+        IAsyncAction any = winrt::when_any(when_signaled(first_event), when_signaled(second_event));
+
+        // Make sure we're still waiting.
+        Sleep(100);
+        REQUIRE(any.Status() == AsyncStatus::Started);
+
+        // Allow only one of the async objects to complete.
+        SetEvent(second_event.get());
+
+        // This should now complete.
+        any.get();
+
+        SetEvent(first_event.get());
+    }
 }
