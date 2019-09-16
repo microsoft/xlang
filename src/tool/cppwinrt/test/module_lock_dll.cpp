679ab333148bc8c25570b1c6f3ddce1d660e3a35
Kenny Kerr <kekerr@microsoft.com>
2019-09-16 12:00:53 -0700

    testing

--- src/tool/cppwinrt/test/module_lock_dll.cpp
+++ src/tool/cppwinrt/test/module_lock_dll.cpp
@@ -2,7 +2,18 @@
 
 // The default behavior (no macro) provides the static winrt::get_module_lock implementation for components/DLLs.
 
-#include "winrt/base.h"
+#include "winrt/Windows.Foundation.h"
+
+namespace
+{
+    struct Stringable : winrt::implements<Stringable, winrt::Windows::Foundation::IStringable>
+    {
+        winrt::hstring ToString()
+        {
+            return L"Stringable";
+        }
+    };
+}
 
 TEST_CASE("module_lock_dll")
 {
@@ -13,6 +24,27 @@ TEST_CASE("module_lock_dll")
     REQUIRE(winrt::get_module_lock() == count + 1);
 
     --winrt::get_module_lock();
+
+    REQUIRE(winrt::get_module_lock() == count);
+
+    {
+        auto stringable = winrt::make<Stringable>();
+        REQUIRE(winrt::get_module_lock() == count + 1);
+    }
+
+    REQUIRE(winrt::get_module_lock() == count);
+
+    {
+        winrt::Windows::Foundation::EventHandler<int> delegate = [](auto&&...) {};
+        REQUIRE(winrt::get_module_lock() == count + 1);
+    }
+
+    REQUIRE(winrt::get_module_lock() == count);
+
+    {
+        winrt::delegate<void()> delegate = [] {};
+        REQUIRE(winrt::get_module_lock() == count + 1);
+    }
 
     REQUIRE(winrt::get_module_lock() == count);
 }
