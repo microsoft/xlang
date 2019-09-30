cbe2f906e1698ef2f3faf752948b0b165bff2e91
Kenny Kerr <kekerr@microsoft.com>
2019-09-30 09:31:54 -0700

    compat

--- src/tool/cppwinrt/cppwinrt/file_writers.h
+++ src/tool/cppwinrt/cppwinrt/file_writers.h
@@ -81,6 +81,18 @@ namespace xlang
         w.write_each<write_category>(members.enums, "enum_category");
         w.write_each<write_struct_category>(members.structs);
         w.write_each<write_category>(members.delegates, "delegate_category");
+
+        // Class names are always required for activation.
+        // Class, enum, and struct names are required for producing GUIDs for generic types.
+        // Interface and delegates names are not required by WinRT.
+        w.write_each<write_name>(members.classes);
+        w.write_each<write_name>(members.enums);
+        w.write_each<write_name>(members.structs);
+        write_lean_and_mean(w);
+        w.write_each<write_name>(members.interfaces);
+        w.write_each<write_name>(members.delegates);
+        write_endif(w);
+
         w.write_each<write_guid>(members.interfaces);
         w.write_each<write_guid>(members.delegates);
         w.write_each<write_default_interface>(members.classes);
@@ -166,18 +178,6 @@ namespace xlang
         w.write_each<write_delegate_implementation>(members.delegates);
         w.write_each<write_produce>(members.interfaces);
         w.write_each<write_dispatch_overridable>(members.classes);
-
-        // Class names are always required for activation.
-        // Class, enum, and struct names are required for producing GUIDs for generic types.
-        // Interface and delegates names are not required by WinRT.
-        w.write_each<write_name>(members.classes);
-        w.write_each<write_name>(members.enums);
-        w.write_each<write_name>(members.structs);
-        write_lean_and_mean(w);
-        w.write_each<write_name>(members.interfaces);
-        w.write_each<write_name>(members.delegates);
-        write_endif(w);
-
         write_close_namespace(w);
 
         write_type_namespace(w, ns);
