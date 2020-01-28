bee0b3a2615ca407b8be0627c6157ef03255985f
Eric Johnson <ericjohnson327@gmail.com>
2020-01-28 14:22:02 -0800

    Ejohn/undocked winrt activation/metadata (#631)
    * C++ apps no longer need a code change
    
    * Adding support for RoGetMetaDataFile and RoResolveNamespace
    
    * Updating test component to cppwinrt
    
    * Manifest parameter no longer needed

---
+++ src/UndockedRegFreeWinRT/TestComponent/Class.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "Class.h"
+#include "Class.g.cpp"
+
+namespace winrt::TestComponent::implementation
+{
+    int32_t Class::Apartment()
+    {
+        APTTYPE aptType;
+        APTTYPEQUALIFIER aptQualifier;
+        check_hresult(CoGetApartmentType(&aptType, &aptQualifier));
+        return aptType;
+    }
+
+    void Class::Apartment(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
