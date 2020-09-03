f5ccafed1e0ca365efa01bddd19423026f50c45b
Will Thant <kythant@microsoft.com>
2020-09-03 16:55:09 -0700

    Initial

---
+++ src/UndockedRegFreeWinRT/mwinrtact/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microsoft.Windows
+{
+    static class UndockedRegFreeWinrt
+    {
+        [DllImport("winrtact.dll")]
+        static extern void winrtact_Initialize();
+
+        public static void Initialize()
+        {
+            winrtact_Initialize();
+        }
+    }
+}
