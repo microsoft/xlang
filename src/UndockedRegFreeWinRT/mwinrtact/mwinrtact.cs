fb98828090fcf3140f533a7e31a67d6f9829a85c
Will Thant <kythant@microsoft.com>
2020-09-22 13:47:44 -0700

    preserve sig

--- src/UndockedRegFreeWinRT/mwinrtact/mwinrtact.cs
+++ src/UndockedRegFreeWinRT/mwinrtact/mwinrtact.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Windows
 {
     public static class UndockedRegFreeWinrt
     {
-        [DllImport("winrtact.dll")]
+        [DllImport("winrtact.dll", PreserveSig=false)]
         static extern void winrtact_Initialize();
 
         public static void Initialize()
