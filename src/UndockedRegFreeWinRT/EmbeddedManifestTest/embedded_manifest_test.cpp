d213818d618e0136ea99d77d1c2a24b1a4fb5f41
Will Thant <kythant@microsoft.com>
2020-09-22 17:15:19 -0700

    Add tests for embedded manifest inside dll

--- src/UndockedRegFreeWinRT/EmbeddedManifestTest/embedded_manifest_test.cpp
+++ src/UndockedRegFreeWinRT/EmbeddedManifestTest/embedded_manifest_test.cpp
@@ -9,6 +9,7 @@
 #include <wrl\client.h>
 #include <wrl\wrappers\corewrappers.h>
 #include "winrt\TestComponent.h"
+#include "winrt\EmbeddedTestComponent.h"
 
 #include "../UndockedRegFreeWinRT/extwinrt.h"
 
@@ -16,7 +17,7 @@ using namespace Microsoft::WRL;
 using namespace ABI::Windows::Foundation;
 using namespace Microsoft::WRL::Wrappers;
 
-TEST_CASE("Embedded Manifest Test")
+TEST_CASE("Embedded Exe Manifest Test")
 {
     SECTION("Activate TestComponent.Both")
     {
@@ -38,6 +39,34 @@ TEST_CASE("Embedded Manifest Test")
     {
         winrt::init_apartment(winrt::apartment_type::single_threaded);
         winrt::TestComponent::ClassMta c;
+        REQUIRE(c.Apartment() == APTTYPE_MTA);
+        winrt::clear_factory_cache();
+        winrt::uninit_apartment();
+    }
+}
+
+TEST_CASE("Embedded Dll Manifest Test")
+{
+    SECTION("Activate TestComponent.Both")
+    {
+        winrt::init_apartment(winrt::apartment_type::single_threaded);
+        winrt::EmbeddedTestComponent::ClassBoth c;
+        REQUIRE(c.Apartment() == APTTYPE_MAINSTA);
+        winrt::clear_factory_cache();
+        winrt::uninit_apartment();
+    }
+    SECTION("Activate TestComponent.Both")
+    {
+        winrt::init_apartment(winrt::apartment_type::single_threaded);
+        winrt::EmbeddedTestComponent::ClassSta c;
+        REQUIRE(c.Apartment() == APTTYPE_MAINSTA);
+        winrt::clear_factory_cache();
+        winrt::uninit_apartment();
+    }
+    SECTION("Activate TestComponent.MTA")
+    {
+        winrt::init_apartment(winrt::apartment_type::single_threaded);
+        winrt::EmbeddedTestComponent::ClassMta c;
         REQUIRE(c.Apartment() == APTTYPE_MTA);
         winrt::clear_factory_cache();
         winrt::uninit_apartment();
