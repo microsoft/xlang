c1293113594861a9e8f9f3331a48d5c5b7c8ecfa
Ben Kuhn <benkuhn@ntdev.microsoft.com>
2020-10-31 10:43:22 -0700

    first stage - pywinrt builds and runs

---
+++ src/pywinrt/pywinrt/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define NOMINMAX
+
+#include "cmd_reader.h"
+#include "meta_reader.h"
+#include "task_group.h"
+#include "text_writer.h"
+
+#include <set>
+
+// TODO: fix version string
+#define XLANG_VERSION_STRING "placeholder"
