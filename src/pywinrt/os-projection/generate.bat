93e68cec5232a78df3251e2c241dae22a90f6d32
Ben Kuhn <benkuhn@ntdev.microsoft.com>
2020-10-31 11:35:45 -0700

    generating files

--- src/pywinrt/os-projection/generate.bat
+++ src/pywinrt/os-projection/generate.bat
@@ -1,11 +1,11 @@
-@echo off
+@echo on
 :: Generate the Windows Runtime Projection DLL
 :: Usage
 ::   generate.bat <pywinrt folder> <cppwinrt folder> <output path>
 
-set windows_sdk='10.0.19041.0'
+set windows_sdk=10.0.19041.0
 
-set pywirnt_path="%~1"
+set pywinrt_path="%~1"
 set cppwinrt_path="%~2"
 set output_path="%~3"
 
@@ -13,15 +13,16 @@ set pywinrt_exe=%pywinrt_path%\pywinrt.exe
 set cppwinrt_exe=%cppwinrt_path%\cppwinrt.exe
 
 :: clean output folder
-:: erase /s /q %output_path%\*
-
-%cppwinrt_exe% -input %windows_sdk% -output %output_path%\cppwinrt -verbose
+:: TODO: fake version...
+echo erase /s /q %output_path%\*
 
 :: TODO: swap this later once basically working.
 :: set %namespaces%="Windows."
-set %namespaces%="-include Windows.Data.Json -include Windows.Devices.Geolocation -include Windows.Foundation -include Windows.Graphics.DirectX"
-set %exclude%="-exclude Windows.UI.Composition -exclude Windows.UI.Xaml"
+set namespaces=-include Windows.Data.Json -include Windows.Devices.Geolocation -include Windows.Foundation -include Windows.Graphics.DirectX
+set exclude=-exclude Windows.UI.Composition -exclude Windows.UI.Xaml
 
-$pyparams = ("-input", $windows_sdk, "-output", $pywinrt_path, "-verbose") + $pyin + $pyout
 
-%pywinrt_exe% -input %windows_sdk% %namespaces% %exclude% -output %output_path% -verbose 
+echo %cppwinrt_exe% -input %windows_sdk% -output %output_path%\cppwinrt -verbose
+%cppwinrt_exe% -input %windows_sdk% -output %output_path%\cppwinrt -verbose
+echo %pywinrt_exe% -input %windows_sdk% %namespaces% %exclude% -output %output_path% -verbose 
+%pywinrt_exe% -input %windows_sdk% %namespaces% %exclude% -output %output_path%\pywinrt -verbose 
